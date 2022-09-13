91018a1c399876bba372bf180fb789c72377994c
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-09-13 22:04:37 +0200

    Added first infrastructure for automatic version increment.

--- .github/workflows/check-software-changes.sh
+++ .github/workflows/check-software-changes.sh
@@ -3,12 +3,22 @@
 # Copyright (C) 2022 Andrea Ballestrazzi
 
 # This is the input file where the file changes are.
-input_file="./bin/$1_changes.txt"
+input_file="./build/$1_changes.txt"
 
 # Now we load the files lines (the changed files) and if we find
 # files whose path starts with src/$1 then we increment the software
 # version.
+changes_found=false
 while IFS= read -r line
 do
-    echo "$line"
+    if [ [["$line" == "src/$1/"*]] ]
+    then
+        echo "Found!"
+        changes_found=true
+        break
+    fi
 done < "$input_file"
+
+if [ "$changes_found" = false ] ; then
+    echo "[INFO] => No changes found for this component."
+fi
