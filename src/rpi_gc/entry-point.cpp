101c26bb6ac8a320409bb8b529a757cfd67475d1
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-09-13 19:57:03 +0200

    Added entry-point.cpp file for the greenhouse controller application.

---
+++ src/rpi_gc/entry-point.cpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+
+// This is the entry point of the application. Here, it starts
+// the main execution of the greenhouse controller.
+int main(int argc, char* argv[]) {
+    return 0;
+}
