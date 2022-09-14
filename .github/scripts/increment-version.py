c5153a7af46b1a2964cc2110f959f4cc6e0097ed
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-09-14 22:30:13 +0200

    Added version increment serialization to python script.

--- .github/scripts/increment-version.py
+++ .github/scripts/increment-version.py
@@ -26,8 +26,11 @@ class ConfigurationFileController:
         self.componentName = componentName
         self.fileLines = []
 
-        # We initialize the version to an invalid state.
-        self.versionNumbers = ComponentVersion(-1, -1, -1)
+        # The following attributes represent the lines where
+        # the macros are defined inside the given file.
+        self.majorMacroDefLine = -1
+        self.minorMacroDefLine = -1
+        self.patchMacroDefLine = -1
 
     def loadVersionNumbers(self) -> ComponentVersion:
         # 1. We read the lines from the file.
@@ -47,6 +50,7 @@ class ConfigurationFileController:
         version_minor = -1
         version_patch = -1
 
+        current_file_line = 0
         for line in self.fileLines:
             # We split up the line so we can find if some string matches the
             # expected one.
@@ -57,14 +61,33 @@ class ConfigurationFileController:
             if len(words) >= 3:
                 if(words[1] == major_macro_name):
                     version_major = int(words[2])
+                    self.majorMacroDefLine = current_file_line
 
                 if(words[1] == minor_macro_name):
                     version_minor = int(words[2])
+                    self.minorMacroDefLine = current_file_line
 
                 if(words[1] == patch_macro_name):
                     version_patch = int(words[2])
+                    self.patchMacroDefLine = current_file_line
+
+            current_file_line += 1
 
         return ComponentVersion(version_major, version_minor, version_patch)
+
+    def commitNewVersion(self, newVersion):
+        [major, minor, patch] = newVersion.getVersionNumbers()
+
+        # Before writing the new version inside the file we need
+        # to update the lines with the new version numbers.
+        self.fileLines[self.majorMacroDefLine] = f"#define {self.componentName}_VERSION_MAJOR {major}\n"
+        self.fileLines[self.minorMacroDefLine] = f"#define {self.componentName}_VERSION_MINOR {minor}\n"
+        self.fileLines[self.patchMacroDefLine] = f"#define {self.componentName}_VERSION_PATCH {patch}\n"
+
+        with open(self.fileName, "w") as configFile:
+            configFile.writelines(self.fileLines)
+
+        configFile.close()
 
 # In this case:
 #   - argv[1] = Component configuration file.
@@ -78,7 +101,7 @@ class ConfigurationFileController:
 #   - <COMP_NAME>_VERSION_MINOR
 #   - <COMP_NAME>_VERSION_PATCH
 # If no option is given, then the patch is automatically increased.
-def IncrementVersion(argv):
+def IncrementVersion(argv) -> ComponentVersion:
     assert len(argv) >= 3, "You need to provide at least the configuration file and the component name."
     bIncrementMajor = False
     bIncrementMinor = False
@@ -100,7 +123,21 @@ def IncrementVersion(argv):
     # Now we load the version from the file controller
     compCurrentVersion = configFileController.loadVersionNumbers()
 
-    return
+    # Now that we have the component we can increment the version
+    if bIncrementMajor:
+        compCurrentVersion.incrementMajor(1)
+    if bIncrementMinor:
+        compCurrentVersion.incrementMinor(1)
+    if bIncrementPatch:
+        compCurrentVersion.incrementPatch(1)
+
+    # And now we can write out the changes to the config file.
+    configFileController.commitNewVersion(compCurrentVersion)
+
+    return compCurrentVersion
 
 if __name__ == "__main__":
-    IncrementVersion(sys.argv)
+    [newMajor, newMinor, newPatch] = IncrementVersion(sys.argv).getVersionNumbers()
+
+    print(f"[INFO] => Increment {sys.argv[2]} version to {newMajor}.{newMinor}.{newPatch}.")
+    print(f"[INFO] => Version changes have been written inside {sys.argv[1]}.")
