6d5e5d64d84a33df4a65ec929f4dc85cc041b3d6
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-09-14 22:35:07 +0200

    Added RPI_GC config file.

---
+++ src/rpi_gc/rpi_gc-config-file.hpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+
+#ifndef RPI_GC_CONFIG_FILE
+#define RPI_GC_CONFIG_FILE
+
+#define rpi_gc_VERSION_MAJOR 0
+#define rpi_gc_VERSION_MINOR 0
+#define rpi_gc_VERSION_PATCH 0
+
+#endif // TEST_CONFIG_FILE_HPP
