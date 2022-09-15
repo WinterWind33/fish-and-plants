19ad21bc8e22fa9eb0e455b073f429b884006602
Andrea Ballestrazzi <85613584+WinterWind33@users.noreply.github.com>
2022-09-15 19:31:55 +0200

    Add basic DevOps infrastructure (#2)
    * Added principal CMakeLists.txt file.
    
    * Updated CMakeLists.txt file.
    
    * Added entry-point.cpp file for the greenhouse controller application.
    
    * Added cmake file for rpi_gc application.
    
    * Added initial github workflow for builds and testing.
    
    * Updated pre-commit hooks.
    
    * Run pre-commit hooks on all the repository.
    
    * Added Catch2 as testing framework.
    
    * Fixed windows compilation output folder and added tests execution to GitHub workflow.
    
    * Fixed typo.
    
    * Fixed typo.
    
    * Changed tests execution on github workflow.
    
    * Added temporary test case to debug workflows.
    
    * Added first version of check software version script.
    
    * Added first infrastructure for automatic version increment.
    
    * Fixed missing dir.
    
    * Added execution permission to scripts.
    
    * Added scripts for github.
    
    * Added first prototype of version increment.
    
    * Added version increment serialization to python script.
    
    * Added RPI_GC config file.
    
    * Updated increment version calls.
    
    * Added debug print string.
    
    * Changed output mode.
    
    * Added lines print for debug.
    
    * Added git reflog output.
    
    * Added fetch depth.
    
    * Added test change.
    
    * Added debug info and fixed script.
    
    * Added git commit and push to increment version script.
    
    * Updated increment version python script.
    
    * Updated increment versions scripts.
    
    * Updated git command.
    
    * Updated increment versions scripts.
    
    * Added first attempt to git commit.
    
    * [rpi_gc](INCREMENT VERSION) Automatic version increment.
    
    * Added CWD print inside python script.
    
    * Test change.
    
    * [rpi_gc](INCREMENT VERSION) Automatic version increment.
    
    * Updated automatic version increment scripts.
    
    * [IV_BOT](Version Increment) Updated rpi_gc version to 0.0.4.
    
    * Configured increment version job to run only on pull request merged into main.
    
    Co-authored-by: IV_BOT <WinterWind33@users.noreply.github.com>

---
+++ .github/scripts/increment-version.py
@@ -0,0 +1,170 @@
+# Copyright (C) 2022 Andrea Ballestrazzi
+import sys
+import copy
+import os
+
+# Represents the version of our component (component = sub software)
+class ComponentVersion:
+    def __init__(self, major, minor, patch):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    def incrementMajor(self, n):
+        self.major += n
+
+    def incrementMinor(self, n):
+        self.minor += n
+
+    def incrementPatch(self, n):
+        self.patch += n
+
+    def getVersionNumbers(self):
+        return [self.major, self.minor, self.patch]
+
+    def equals(self, otherVersion):
+        return self.major == otherVersion.major and self.minor == otherVersion.minor and self.patch == otherVersion.patch
+
+class ConfigurationFileController:
+    def __init__(self, configFileName, componentName):
+        self.fileName = configFileName
+        self.componentName = componentName
+        self.fileLines = []
+
+        # The following attributes represent the lines where
+        # the macros are defined inside the given file.
+        self.majorMacroDefLine = -1
+        self.minorMacroDefLine = -1
+        self.patchMacroDefLine = -1
+
+    def loadVersionNumbers(self) -> ComponentVersion:
+        # 1. We read the lines from the file.
+        with open(self.fileName, "r") as inputFile:
+            self.fileLines = inputFile.readlines()
+
+        inputFile.close()
+
+        # Now that we have the lines we can start searching for the string
+        # that represents our macros. Here we create the names of the macros
+        # that we need to find.
+        major_macro_name = f"{self.componentName}_VERSION_MAJOR"
+        minor_macro_name = f"{self.componentName}_VERSION_MINOR"
+        patch_macro_name = f"{self.componentName}_VERSION_PATCH"
+
+        version_major = -1
+        version_minor = -1
+        version_patch = -1
+
+        current_file_line = 0
+        for line in self.fileLines:
+            # We split up the line so we can find if some string matches the
+            # expected one.
+            words = line.split(" ")
+
+            # We expect something like:
+            # cpp: #define <COMP_NAME>_VERSION_MAJOR 3
+            if len(words) >= 3:
+                if(words[1] == major_macro_name):
+                    version_major = int(words[2])
+                    self.majorMacroDefLine = current_file_line
+
+                if(words[1] == minor_macro_name):
+                    version_minor = int(words[2])
+                    self.minorMacroDefLine = current_file_line
+
+                if(words[1] == patch_macro_name):
+                    version_patch = int(words[2])
+                    self.patchMacroDefLine = current_file_line
+
+            current_file_line += 1
+
+        return ComponentVersion(version_major, version_minor, version_patch)
+
+    def commitNewVersion(self, newVersion):
+        [major, minor, patch] = newVersion.getVersionNumbers()
+
+        # Before writing the new version inside the file we need
+        # to update the lines with the new version numbers.
+        self.fileLines[self.majorMacroDefLine] = f"#define {self.componentName}_VERSION_MAJOR {major}\n"
+        self.fileLines[self.minorMacroDefLine] = f"#define {self.componentName}_VERSION_MINOR {minor}\n"
+        self.fileLines[self.patchMacroDefLine] = f"#define {self.componentName}_VERSION_PATCH {patch}\n"
+
+        with open(self.fileName, "w") as configFile:
+            configFile.writelines(self.fileLines)
+
+        configFile.close()
+
+# In this case:
+#   - argv[1] = Component configuration file.
+#   - argv[2] = Component name.
+#   - argv[2..N] = versions number to update. Could be:
+#       1. -M = increment major number.
+#       2. -m = increment minor number.
+#       3. -p = increment patch number.
+# For now, the file should have he following macros for the versions numbers:
+#   - <COMP_NAME>_VERSION_MAJOR
+#   - <COMP_NAME>_VERSION_MINOR
+#   - <COMP_NAME>_VERSION_PATCH
+# If no option is given, then the patch is automatically increased.
+def IncrementVersion(argv, beforeVersion) -> ComponentVersion:
+    assert len(argv) >= 3, "You need to provide at least the configuration file and the component name."
+    bIncrementMajor = False
+    bIncrementMinor = False
+    bIncrementPatch = False
+
+    # Here we check which version number we need to increment
+    if len(argv) > 3:
+        for i in range(3, len(argv)):
+            bIncrementMajor = (bIncrementMajor or (argv[i] == "-M"))
+            bIncrementMinor = (bIncrementMinor or (argv[i] == "-m"))
+            bIncrementPatch = (bIncrementPatch or (argv[i] == "-p"))
+
+    # If none of the above flags are checked, we increment the patch number by default.
+    if not bIncrementMajor and not bIncrementMinor and not bIncrementPatch:
+        bIncrementPatch = True
+
+    newVersion = copy.deepcopy(beforeVersion)
+
+    # Now that we have the component we can increment the version
+    if bIncrementMajor:
+        newVersion.incrementMajor(1)
+    if bIncrementMinor:
+        newVersion.incrementMinor(1)
+    if bIncrementPatch:
+        newVersion.incrementPatch(1)
+
+    return newVersion
+
+if __name__ == "__main__":
+    print(f"[INFO] => Python script working dir: {os.getcwd()}")
+
+    # We need to load up the current version before incrementing it.
+    configFileController = ConfigurationFileController(sys.argv[1], sys.argv[2])
+    beforeVersion = configFileController.loadVersionNumbers()
+    [beforeMajor, beforeMinor, beforePatch] = beforeVersion.getVersionNumbers()
+
+    # If we get -1.-1.-1 we had a problem loading the numbers.
+    assert beforeMajor >= 0 and beforeMinor >= 0 and beforePatch >= 0
+
+    print(f"[INFO] => Found version {beforeMajor}.{beforeMinor}.{beforePatch}.")
+    # Now we can increment the version according to the desired given options.
+
+    incrementedVersion = IncrementVersion(sys.argv, beforeVersion)
+    if incrementedVersion.equals(beforeVersion):
+        print(f"[ERROR] => Could not increment the version. Skipping changes commit.")
+        exit(1)
+
+    configFileController.commitNewVersion(incrementedVersion)
+    [afterMajor, afterMinor, afterPatch] = incrementedVersion.getVersionNumbers()
+
+    print(f"[INFO] => Incremented {sys.argv[2]} version to {afterMajor}.{afterMinor}.{afterPatch}.")
+    print(f"[INFO] => Version changes have been written to {sys.argv[1]}.")
+
+    # Now we save the version string into a file so we can retrieve it later
+    # for the git commit.
+    with open("./tmp/new_version.txt", "a") as newVersionFile:
+        newVersionFile.write(f"{afterMajor}.{afterMinor}.{afterPatch}\n")
+
+    newVersionFile.close()
+
+    print("[INFO] => The new version has been written to new_version.txt file.")
---
+++ test/test-app-entry-point.cpp
@@ -0,0 +1,6 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Useless test case", "[unit][integration]") {
+    CHECK(true);
+}
