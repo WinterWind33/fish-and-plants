1c37b7ef14e93fda599751e1c209c0aa19a3adfd
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-09-26 21:30:48 +0200

    Added first implementation of SPLLogger wrapper class.

--- src/wrappers/gh_log/spl-logger.hpp
+++ src/wrappers/gh_log/spl-logger.hpp
@@ -5,9 +5,35 @@
 #ifdef USE_SPDLOG
 #include <spdlog/spdlog.h>
 
-#include <spl-logger.h>
+#include <logger.hpp>
 
-namespace gh_log {} // namespace gh_log
+// C++ STL
+#include <memory>
+
+namespace gh_log {
+
+    class SPLLogger : public Logger {
+    public:
+        using logger_type = spdlog::logger;
+        using logger_pointer = std::shared_ptr<logger_type>;
+        using sink_type = spdlog::sinks::sink;
+        using sink_pointer = std::shared_ptr<sink_type>;
+
+        SPLLogger(logger_pointer logger);
+        ~SPLLogger() noexcept override = default;
+
+        void logTrace(const LogStringType& msg) override;
+        void logDebug(const LogStringType& msg) override;
+        void logInfo(const LogStringType& msg) override;
+        void logWarning(const LogStringType& msg) override;
+        void logError(const LogStringType& msg) override;
+        void logCritical(const LogStringType& msg) override;
+
+    private:
+        logger_pointer m_logger{};
+    };
+
+} // namespace gh_log
 
 #endif // USE_SPDLOG
 #endif // !SPL_LOGGER_HPP
