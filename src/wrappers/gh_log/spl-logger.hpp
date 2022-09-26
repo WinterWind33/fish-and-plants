a0651f6811ce0d6ab673b75c908819e81353e7f8
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-09-26 21:03:34 +0200

    Added spl-logger.hpp header file.

---
+++ src/wrappers/gh_log/spl-logger.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef SPL_LOGGER_HPP
+#define SPL_LOGGER_HPP
+
+#ifdef USE_SPDLOG
+#include <spdlog/spdlog.h>
+
+#include <spl-logger.h>
+
+namespace gh_log {} // namespace gh_log
+
+#endif // USE_SPDLOG
+#endif // !SPL_LOGGER_HPP
