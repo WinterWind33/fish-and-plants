94f04791129ced27b54aeda9c0c158a5f8cc5f4c
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-03 22:01:41 +0200

    Added HALContextMock class and hal-module-tests.cpp file.

---
+++ test/gh_hal/hal-module-tests.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#include <catch2/catch_test_macros.hpp>
+
+// Test doubles
+#include <gh_hal/test-doubles/hal-context.mock.hpp>
+
+TEST_CASE("HALModule Unit Tests", "[unit][gh_hal][HALModule]") {
+
+}
---
+++ test/gh_hal/test-doubles/hal-context.mock.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef HAL_CONTEXT_MOCK_HPP
+#define HAL_CONTEXT_MOCK_HPP
+
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif
+
+#include <hal-context.hpp>
+
+namespace gh_hal::mocks {
+
+class HALContextMock : public gh_hal::HALContext {
+public:
+    ~HALContextMock() noexcept override = default;
+
+    MOCK_METHOD((const logger_pointer&), getLogger, (), (const, noexcept, override));
+    MOCK_METHOD(bool, isSimulation, (), (const, noexcept, override));
+};
+
+} // namespace mocks
+
+#endif //! HAL_CONTEXT_MOCK_HPP
