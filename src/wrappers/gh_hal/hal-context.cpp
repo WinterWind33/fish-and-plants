1d3b37e58cad5ef23dd6bdb5c2003a1867f7143a
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-03 20:22:51 +0200

    Added first implementation of HALContext class.

--- src/wrappers/gh_hal/hal-context.cpp
+++ src/wrappers/gh_hal/hal-context.cpp
@@ -1,2 +1,9 @@
 // Copyright (c) 2022 Andrea Ballestrazzi
 #include <hal-context.hpp>
+
+namespace gh_hal {
+
+    HALContext::HALContext(logger_pointer logger) noexcept :
+        m_logger{std::move(logger)} {}
+
+} // namespace gh_hal
--- src/wrappers/gh_hal/hal-context.hpp
+++ src/wrappers/gh_hal/hal-context.hpp
@@ -2,4 +2,28 @@
 #ifndef HAL_CONTEXT_HPP
 #define HAL_CONTEXT_HPP
 
+#include <logger.hpp>
+
+// C++ STL
+#include <memory>
+
+namespace gh_hal {
+
+    class HALContext {
+    public:
+        using logger_pointer = std::shared_ptr<gh_log::Logger>;
+
+        HALContext(logger_pointer logger) noexcept;
+        virtual ~HALContext() noexcept = default;
+
+        virtual const logger_pointer& getLogger() const noexcept {
+            return m_logger;
+        }
+
+    private:
+        logger_pointer m_logger{};
+    };
+
+} // namespace gh_hal
+
 #endif // !HAL_CONTEXT_HPP
