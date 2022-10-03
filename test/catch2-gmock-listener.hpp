7dc763607e71b3c4ef75c8918c87f10576d4f582
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-03 21:45:02 +0200

    Added Catch2 main and Catch2 assertion handler.

--- test/catch2-gmock-listener.hpp
+++ test/catch2-gmock-listener.hpp
@@ -4,5 +4,23 @@
 
 #if defined(USE_CATCH2_AS_TESTING_FRAMEWORK) && defined(USE_GMOCK)
 
+#include <catch2/catch_session.hpp>
+#include <gmock/gmock.h>
+
+//! \brief Represents the listener used by GMock when using its
+//!  constructs to do tests (e.g. EXPECT_CALL, ON_CALL etc...)
+//!	 This basically inherits from a defaulted event listener.
+class GMCatch2Listener : public testing::EmptyTestEventListener {
+public:
+
+    //! \brief Called after test partial result. This will transform
+    //!	 GMock tests data into Catch2 test data that can be used by
+    //!  Catch2 to provide feedback and fail tests.
+    //!
+    //! \param result The GMock test partial result data that needs to be
+    //!	 converted into Catch2 data.
+    void OnTestPartResult(const testing::TestPartResult& result) override;
+};
+
 #endif // USE_CATCH2_AS_TESTING_FRAMEWORK && USE_GMOCK
 #endif // !CATCH2_GMOCK_LISTENER_HPP
--- test/test-app-entry-point.cpp
+++ test/test-app-entry-point.cpp
@@ -1,6 +1,68 @@
-// Copyright (C) 2022 Andrea Ballestrazzi
-#include <catch2/catch_test_macros.hpp>
+// Copyright (c) 2022 Andrea Ballestrazzi
+#include <catch2-gmock-listener.hpp>
 
-TEST_CASE("Useless test case", "[unit][integration]") {
-    CHECK(true);
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif // USE_GMOCK
+
+// C++ STL
+#include <cstdint>
+#include <iostream>
+#include <memory>
+
+//! \brief Utility function used to sink a pointer, i.e. delete it and set it to nullptr.
+//!  If the passed pointer is nullptr or the pointed pointer is nullptr, nothing will happen.
+//!
+//! \tparam T The type of the pointed-to value by *pointer that needs to be deleted.
+//! \param pointer The pointer-to the pointed value that needs to be deleted.
+template<typename T>
+static void SinkPointer(T** pointer) {
+    if (pointer == nullptr || *pointer == nullptr)
+        return;
+
+    delete *pointer;
+    *pointer = nullptr;
+}
+
+int main(int argc, char* argv[]) {
+    std::unique_ptr<Catch::Session> catch2Session{};
+
+    // Before starting Catch2, we need to initialize
+    // GMock library. In order to do so, we need to call
+    // `testing::InitGoogleMock` but we DO NOT need to pass every argument
+    // to GMock. We build the following int variable so it doesn't
+    // parse every argument, just the first one.
+    std::int32_t gmockArgC = 1;
+    testing::InitGoogleMock(&gmockArgC, argv);
+
+    testing::UnitTest* gmockUnitTestInstance = testing::UnitTest::GetInstance();
+    if (!gmockUnitTestInstance) {
+        std::cerr << "[FATAL] => Failed to initialize GMock: unit test instance in NULL." << std::endl;
+
+        // INVALID_HANDLE error
+        return 6;
+    }
+
+    testing::TestEventListeners& listeners = gmockUnitTestInstance->listeners();
+    testing::TestEventListener* defaultResultPrinter = listeners.default_result_printer();
+
+    // We can happend our Catch2 listener.
+    listeners.Append(new GMCatch2Listener{});
+
+    // Here we need to remove the default result listener, as we are providing
+    // our own listener.
+    // NOTE: when a GMock listener is released, we become the owner, so we need to delete the
+    // released listener.
+    testing::TestEventListener* releasedListener = listeners.Release(defaultResultPrinter);
+    SinkPointer(&releasedListener);
+    defaultResultPrinter = nullptr;
+
+    catch2Session = std::make_unique<Catch::Session>();
+    if (!catch2Session) {
+        std::cerr << "[FATAL] => Cannot create Catch2 session, system internal error." << std::endl;
+        return 6;
+    }
+
+    catch2Session->applyCommandLine(argc, argv);
+    return catch2Session->run();
 }
