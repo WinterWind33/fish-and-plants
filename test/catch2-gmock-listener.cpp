c77a1243d6f5297010135e4b3e68115775824393
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-03 21:29:42 +0200

    Added conditional macro for test app to exclude Catch2 listener on GMock from compilation when not using one of them.

--- test/catch2-gmock-listener.cpp
+++ test/catch2-gmock-listener.cpp
@@ -1,2 +1,6 @@
 // Copyright (c) 2022 Andrea Ballestrazzi
 #include <catch2-gmock-listener.hpp>
+
+#if defined(USE_CATCH2_AS_TESTING_FRAMEWORK) && defined(USE_GMOCK)
+
+#endif // USE_CATCH2_AS_TESTING_FRAMEWORK && USE_GMOCK
