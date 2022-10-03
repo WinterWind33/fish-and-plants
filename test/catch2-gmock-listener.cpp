2a050f11c5dbefeeb7b3d6309ab64322cf1391e9
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-03 21:23:39 +0200

    Added catch2 gmock listener files.

---
+++ test/catch2-gmock-listener.cpp
@@ -0,0 +1,2 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#include <catch2-gmock-listener.hpp>
---
+++ test/catch2-gmock-listener.hpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef CATCH2_GMOCK_LISTENER_HPP
+#define CATCH2_GMOCK_LISTENER_HPP
+
+#endif // !CATCH2_GMOCK_LISTENER_HPP
