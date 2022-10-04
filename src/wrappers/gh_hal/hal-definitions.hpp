bc72f5b6848df0d9b8e236fa0022d831f674c649
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-04 22:01:22 +0200

    Added hal-definitions.hpp header file.

---
+++ src/wrappers/gh_hal/hal-definitions.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef HAL_DEFINITIONS_HPP
+#define HAL_DEFINITIONS_HPP
+
+// C++ STL
+#include <cstdint>
+
+namespace gh_hal {
+
+    //! \brief Represents the ID of a PIN inside the used board.
+    using PinID = std::uint32_t;
+
+} // namespace gh_hal
+
+#endif // !HAL_DEFINITIONS_HPP
