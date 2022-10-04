0a4392edc137e6a8486eb6e92c042dcac00af46e
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-04 18:32:19 +0200

    Deleted unused unit test file and updated includes.

--- test/gh_hal/hal-module-tests.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 Andrea Ballestrazzi
-#include <catch2/catch_test_macros.hpp>
-
-// Test doubles
-#include <gh_hal/test-doubles/hal-context.mock.hpp>
-
-TEST_CASE("HALModule Unit Tests", "[unit][gh_hal][HALModule]") {
-
-}
