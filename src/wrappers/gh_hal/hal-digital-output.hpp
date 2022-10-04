a19a6c89db5036d7412e54c063a12150429cbce1
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-04 22:06:26 +0200

    Added HALDigitalOutput interface.

--- src/wrappers/gh_hal/hal-digital-output.hpp
+++ src/wrappers/gh_hal/hal-digital-output.hpp
@@ -2,6 +2,17 @@
 #ifndef HAL_DIGITAL_OUTPUT_HPP
 #define HAL_DIGITAL_OUTPUT_HPP
 
+#include <hal-definitions.hpp>
 
+namespace gh_hal {
+
+    struct HALDigitalOutput {
+        virtual ~HALDigitalOutput() noexcept = default;
+
+        virtual void turnOn() noexcept = 0;
+        virtual void turnOff() noexcept = 0;
+    };
+
+} // namespace gh_hal
 
 #endif // !HAL_DIGITAL_OUTPUT_HPP
