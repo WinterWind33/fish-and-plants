41bf347f3853c6047c3e11efbe25fc0e8d876fd0
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-20 18:12:15 +0200

    Updated CMake configuration.

--- src/wrappers/gh_hal/hal-board-pin.hpp
+++ src/wrappers/gh_hal/hal-board-pin.hpp
@@ -6,7 +6,7 @@
 #include <gh_hal/hal-digital-output.hpp>
 
 #ifdef USE_CPPGPIO
-#include <../third-party/CppGPIO/include/cppgpio.hpp>
+#include <cppgpio.hpp>
 #else
 #include <chrono>
 #endif // USE_CPPGPIO
