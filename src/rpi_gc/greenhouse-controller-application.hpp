2c790f2d14b78ad5da99039affdc8bf9528f3c6d
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-24 19:11:33 +0200

    Added greenhouse-controller-application.hpp file.

---
+++ src/rpi_gc/greenhouse-controller-application.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+#ifndef GREENHOUSE_CONTROLLER_APPLICATION_HPP
+#define GREENHOUSE_CONTROLLER_APPLICATION_HPP
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !GREENHOUSE_CONTROLLER_APPLICATION_HPP
