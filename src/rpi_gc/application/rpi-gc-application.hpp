0ce5da2044a4bf8500b6a8fb0fc7c8e2587a2fce
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-24 19:04:15 +0200

    Added rpi-gc-application.hpp header file.

---
+++ src/rpi_gc/application/rpi-gc-application.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+#ifndef RPI_GC_APPLICATION_HPP
+#define RPI_GC_APPLICATION_HPP
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !RPI_GC_APPLICATION_HPP
