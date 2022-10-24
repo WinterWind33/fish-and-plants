6ad3d4f79748ff89b2fd0bf41911aaeac56778a8
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-24 19:08:05 +0200

    Added Application interface.

--- src/rpi_gc/application/rpi-gc-application.hpp
+++ src/rpi_gc/application/rpi-gc-application.hpp
@@ -2,6 +2,22 @@
 #ifndef RPI_GC_APPLICATION_HPP
 #define RPI_GC_APPLICATION_HPP
 
-namespace rpi_gc {} // namespace rpi_gc
+// C++ STL
+#include <ostream>
+
+namespace rpi_gc {
+    using OutputStream = std::ostream;
+
+    //! \brief Represents the basic interface of an application object that
+    //!  directs the application logic.
+    struct Application {
+        virtual ~Application() noexcept = default;
+
+        //! \brief Runs the application logic.
+        //!
+        virtual void run() noexcept = 0;
+    };
+
+} // namespace rpi_gc
 
 #endif // !RPI_GC_APPLICATION_HPP
