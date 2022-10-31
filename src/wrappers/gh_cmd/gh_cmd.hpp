3aa2766f1bd4f83687baf5ab2e8ccdd7e289fda2
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-10-31 17:59:06 +0100

    Fixed cmake configuration and added gh_cmd.hpp header file.

---
+++ src/wrappers/gh_cmd/gh_cmd.hpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef GH_CMD_HPP
+#define GH_CMD_HPP
+
+#ifdef USE_POPL20
+#include <popl20/popl.hpp>
+#else
+#error "There is no alternative to POPL20 right now, you must choose it."
+#endif
+
+#endif // !GH_CMD_HPP
