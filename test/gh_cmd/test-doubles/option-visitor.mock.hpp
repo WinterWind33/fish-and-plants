280b9ae84f1b2bcc4d3c2162784b2285d382432c
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-01 10:53:41 +0100

    Added option-visitor.mock.hpp header file.

---
+++ test/gh_cmd/test-doubles/option-visitor.mock.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef OPTION_VISITOR_MOCK_HPP
+#define OPTION_VISITOR_MOCK_HPP
+
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif // USE_GMOCK
+
+#include <gh_cmd/gh_cmd.hpp>
+
+namespace gh_cmd::mocks {
+
+} // namespace gh_cmd::mocks
+
+#endif // !OPTION_VISITOR_MOCK_HPP
