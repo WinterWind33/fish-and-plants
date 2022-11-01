b6c61c347b672776c763c956d4f084c5733a9019
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-01 11:31:03 +0100

    Added last switch tests and improved configurations.

--- test/gh_cmd/switch.tests.cpp
+++ test/gh_cmd/switch.tests.cpp
@@ -30,4 +30,56 @@ TEST_CASE("Switch sociable unit tests", "[unit][sociable][gh_cmd][Switch]") {
             CHECK(switchUnderTest.getDescription() == EXPECTED_DESCRIPTION);
         }
     }
+
+    SECTION("Visiting the implementation") {
+        constexpr char DUMMY_SHORT_NAME{'h'};
+        constexpr std::string_view DUMMY_LONG_NAME{"help"};
+        constexpr std::string_view DUMMY_DESCRIPTION{"Produces help message"};
+
+        Switch<char> switchUnderTest{DUMMY_SHORT_NAME, std::string{DUMMY_LONG_NAME}, std::string{DUMMY_DESCRIPTION}};
+
+        SECTION("Method: acceptVisitor() non-const") {
+            using BaseOptionImplType = CommandOption<char>::base_impl_type;
+            using VisitorArgumentType = std::shared_ptr<BaseOptionImplType>;
+            using OptionVisitorMockType = mocks::OptionVisitorMock<VisitorArgumentType>;
+
+            SECTION("Should call the visitor visit() method") {
+                OptionVisitorMockType optionVisitorMock{};
+                EXPECT_CALL(optionVisitorMock, visit).Times(1);
+
+                switchUnderTest.acceptVisitor(optionVisitorMock);
+            }
+
+            SECTION("Should pass to the visit() method a valid pointer") {
+                OptionVisitorMockType optionVisitorMock{};
+                EXPECT_CALL(optionVisitorMock, visit).WillOnce([](VisitorArgumentType option){
+                    CHECK(option != nullptr);
+                });
+
+                switchUnderTest.acceptVisitor(optionVisitorMock);
+            }
+        }
+
+        SECTION("Method: acceptVisitor() const") {
+            using BaseOptionImplType = CommandOption<char>::base_impl_type;
+            using VisitorArgumentType = std::shared_ptr<const BaseOptionImplType>;
+            using OptionVisitorMockType = testing::StrictMock<mocks::ConstOptionVisitorMock<VisitorArgumentType>>;
+
+            SECTION("Should call the visitor visit() method") {
+                OptionVisitorMockType optionVisitorMock{};
+                EXPECT_CALL(optionVisitorMock, visit).Times(1);
+
+                switchUnderTest.acceptVisitor(optionVisitorMock);
+            }
+
+            SECTION("Should pass to the visit() method a valid pointer") {
+                OptionVisitorMockType optionVisitorMock{};
+                EXPECT_CALL(optionVisitorMock, visit).WillOnce([](VisitorArgumentType option){
+                    CHECK(option != nullptr);
+                });
+
+                switchUnderTest.acceptVisitor(optionVisitorMock);
+            }
+        }
+    }
 }
