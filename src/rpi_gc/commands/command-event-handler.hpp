2dd1b0ea231428ea83e698c22251356dcc87f7e2
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-06 14:51:27 +0100

    Added command-event-handler.hpp header file.

---
+++ src/rpi_gc/commands/command-event-handler.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+#ifndef COMMAND_EVENT_HANDLER_HPP
+#define COMMAND_EVENT_HANDLER_HPP
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !COMMAND_EVENT_HANDLER_HPP
