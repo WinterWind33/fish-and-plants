96aa91b04713049ad182eac3ff51676d3505cd1f
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-07 21:49:29 +0100

    Added application-help-command.hpp header file.

---
+++ src/rpi_gc/commands/application-help-command.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+#ifndef APPLICATION_HELP_COMMAND_HPP
+#define APPLICATION_HELP_COMMAND_HPP
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !APPLICATION_HELP_COMMAND_HPP
