3599c17d568adca24c32b4851367f88a13dc6ed6
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-07 21:21:03 +0100

    Renamed file and added Command interface.

--- src/rpi_gc/commands/command-event-handler.hpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2022 Andrea Ballestrazzi
-#ifndef COMMAND_EVENT_HANDLER_HPP
-#define COMMAND_EVENT_HANDLER_HPP
-
-namespace rpi_gc {} // namespace rpi_gc
-
-#endif // !COMMAND_EVENT_HANDLER_HPP
---
+++ src/rpi_gc/commands/command.hpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <common/types.hpp>
+
+namespace rpi_gc {
+
+    //! \brief Represents a command that can be executed. This is the
+    //!  most basic interface for a command.
+    struct Command {
+        virtual ~Command() noexcept = default;
+
+        //! \brief Executes the command.
+        //! \return True if the command execution went without errors, false
+        //!  otherwise.
+        virtual bool execute() noexcept = 0;
+    };
+
+} // namespace rpi_gc
+
+#endif // !COMMAND_HPP
