a744c66f07514ec79ab2f655dcfe4c7949a595f5
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-07 21:55:30 +0100

    Added construct definition.

--- src/rpi_gc/commands/application-help-command.cpp
+++ src/rpi_gc/commands/application-help-command.cpp
@@ -1,4 +1,10 @@
 // Copyright (C) 2022 Andrea Ballestrazzi
 #include <commands/application-help-command.hpp>
 
-namespace rpi_gc {} // namespace rpi_gc
+namespace rpi_gc {
+
+    ApplicationHelpCommand::ApplicationHelpCommand(ostream_ref outputStream, option_parser_ref optionParser) noexcept :
+        m_outputStream{std::move(outputStream)},
+        m_optionParser{std::move(optionParser)} {}
+
+} // namespace rpi_gc
