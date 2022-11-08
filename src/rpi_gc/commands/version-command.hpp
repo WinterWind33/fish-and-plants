ddf1d936678d01e93a8913ce295a9e996e4044a3
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-08 21:54:08 +0100

    Added version-command.hpp header file.

---
+++ src/rpi_gc/commands/version-command.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef VERSION_COMMAND_HPP
+#define VERSION_COMMAND_HPP
+
+#include <commands/terminal-command.hpp>
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !VERSION_COMMAND_HPP
