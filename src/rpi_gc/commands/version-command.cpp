5a641a86174032c3ce399c19c0a1b2abc07dad57
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-08 22:00:14 +0100

    Added version-command.cpp and VersionCommand class.

---
+++ src/rpi_gc/commands/version-command.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#include <commands/version-command.hpp>
+
+namespace rpi_rc {} // namespace rpi_gc
--- src/rpi_gc/commands/version-command.hpp
+++ src/rpi_gc/commands/version-command.hpp
@@ -2,8 +2,30 @@
 #ifndef VERSION_COMMAND_HPP
 #define VERSION_COMMAND_HPP
 
+#include <common/types.hpp>
 #include <commands/terminal-command.hpp>
 
-namespace rpi_gc {} // namespace rpi_gc
+// C++ STL
+#include <functional>
+#include <ostream>
+
+namespace rpi_gc {
+
+    class VersionCommand : public TerminalCommand<CharType> {
+    public:
+        using ostream_ref = std::reference_wrapper<std::ostream>;
+
+        VersionCommand(ostream_ref outputStream) noexcept;
+        ~VersionCommand() noexcept override = default;
+
+        constexpr name_type getName() const noexcept override { return name_type{"version"}; }
+
+        bool processOptions(const options_vector& options,
+            const non_options_vector& nonOptions, const unknown_options_vector& unknown) noexcept override;
+
+        bool execute() noexcept override;
+    };
+
+} // namespace rpi_gc
 
 #endif // !VERSION_COMMAND_HPP
