6497759e7cea83c4f8a0be96dd911fa0e7a0299c
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-09 18:03:13 +0100

    Added help-command.hpp and .cpp files.

---
+++ src/rpi_gc/commands/help-command.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#include <commands/help-command.hpp>
+
+namespace rpi_gc {} // namespace rpi_gc
---
+++ src/rpi_gc/commands/help-command.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef HELP_COMMAND_HPP
+#define HELP_COMMAND_HPP
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !HELP_COMMAND_HPP
