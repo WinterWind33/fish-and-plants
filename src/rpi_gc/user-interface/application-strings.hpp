9b893892b1328283b71a34ab442a7322eb4d1b78
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-15 17:56:42 +0100

    Added application-strings.hpp header file.

---
+++ src/rpi_gc/user-interface/application-strings.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef APPLICATION_STRINGS_HPP
+#define APPLICATION_STRINGS_HPP
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !APPLICATION_STRINGS_HPP
