0fb8cd78d34843c3061712ea0b75a62162d3d6e5
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-15 20:03:43 +0100

    Added commands strings.

--- src/rpi_gc/user-interface/commands-strings.hpp
+++ src/rpi_gc/user-interface/commands-strings.hpp
@@ -3,8 +3,21 @@
 #define COMMANDS_STRINGS_HPP
 
 #include <common/types.hpp>
+#include <user-interface/application-strings.hpp>
 
 namespace rpi_gc::strings::commands {
+
+    constexpr StringViewType HELP{"help"};
+    constexpr StringViewType VERSION{"version"};
+    constexpr StringViewType APPLICATION{strings::application::EXECUTABLE_NAME};
+    constexpr StringViewType EXIT{"exit"};
+
+    namespace feedbacks {
+        constexpr StringViewType TYPE_HELP{"Type \'help\' for a list of the available commands."};
+        constexpr StringViewType UNRECOGNIZED_COMMAND{"Command not recognized."};
+        constexpr StringViewType TEARING_DOWN{"Tearing down..."};
+        constexpr StringViewType GOODBYE{"Goodbye."};
+    } // namespace feedbacks
 
 } // namespace rpi_gc
 
