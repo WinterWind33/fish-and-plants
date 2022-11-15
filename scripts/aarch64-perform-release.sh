2236a525f4309ef299be296fd9c6dd326b7010c2
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-15 20:06:04 +0100

    Added Cortex A53 optimization compilation for aarch64 releases.

--- scripts/aarch64-perform-release.sh
+++ scripts/aarch64-perform-release.sh
@@ -2,5 +2,5 @@
 
 cd buildLinux
 #rm -r -f ./*
-cmake .. -DCOMPILE_AARCH64_ON_LINUX=True --toolchain "toolchains/aarch64-toolchain.cmake"
+cmake .. -DCOMPILE_AARCH64_ON_LINUX=True -DCOMPILE_FOR_CORTEX_A53=True --toolchain "toolchains/aarch64-toolchain.cmake"
 cmake --build . --config Release
