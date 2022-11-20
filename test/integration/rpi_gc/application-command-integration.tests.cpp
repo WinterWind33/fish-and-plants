896b593188ec43a61a2481ddda3393d8c0c7840d
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-20 15:53:01 +0100

    Added application-command-integration.tests.cpp file.

---
+++ test/integration/rpi_gc/application-command-integration.tests.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#include <testing-core.hpp>
+
+#include <commands/application-command.hpp>
+#include <commands/version-command.hpp>
+#include <commands/help-command.hpp>
