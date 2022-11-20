ec1546fc61446576296c2d7cfa6f1af0fead709f
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-20 18:21:29 +0100

    Added automatic-watering-command.hpp header file.

---
+++ src/rpi_gc/commands/automatic-watering/automatic-watering-command.hpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2022 Andrea Ballestrazzi
+#ifndef AUTOMATIC_WATERING_COMMAND_HPP
+#define AUTOMATIC_WATERING_COMMAND_HPP
+
+#include <commands/terminal-command.hpp>
+
+namespace rpi_gc {} // namespace rpi_gc
+
+#endif // !AUTOMATIC_WATERING_COMMAND_HPP
