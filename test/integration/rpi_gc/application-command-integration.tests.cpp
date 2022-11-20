08c54bde20a0c859cf0bb1e775f26ae3b7cba59e
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-20 16:10:49 +0100

    Added integration tests.

--- test/integration/rpi_gc/application-command-integration.tests.cpp
+++ test/integration/rpi_gc/application-command-integration.tests.cpp
@@ -1,6 +1,40 @@
 // Copyright (c) 2022 Andrea Ballestrazzi
 #include <testing-core.hpp>
 
+#include <user-interface/application-strings.hpp>
+
+#include <gh_cmd/gh_cmd.hpp>
 #include <commands/application-command.hpp>
 #include <commands/version-command.hpp>
 #include <commands/help-command.hpp>
+
+TEST_CASE("Bivalent commands and ApplicationCommand integration", "[integration][ApplicationCommand][HelpCommand][VersionCommand]") {
+    using namespace rpi_gc;
+
+    OutputStringStream outputStream{};
+
+    SECTION("ApplicationCommand execution") {
+        gh_cmd::DefaultOptionParser<CharType> optionParser{};
+
+        VersionCommand versionCommand{outputStream};
+        HelpCommand helpCommand{outputStream, std::vector<TerminalCommandType*>{&versionCommand}};
+
+        ApplicationCommand commandUnderTest{outputStream, optionParser};
+        commandUnderTest.addBivalentCommand(versionCommand);
+        commandUnderTest.addBivalentCommand(helpCommand);
+
+        SECTION("Executing version command should halt the execution flow") {
+            commandUnderTest.processInputOptions({StringType{strings::application::EXECUTABLE_NAME}, "--version"});
+
+            const bool bRes{commandUnderTest.execute()};
+            CHECK_FALSE(bRes);
+        }
+
+        SECTION("Executing version command should halt the execution flow") {
+            commandUnderTest.processInputOptions({StringType{strings::application::EXECUTABLE_NAME}, "--help"});
+
+            const bool bRes{commandUnderTest.execute()};
+            CHECK_FALSE(bRes);
+        }
+    }
+}
