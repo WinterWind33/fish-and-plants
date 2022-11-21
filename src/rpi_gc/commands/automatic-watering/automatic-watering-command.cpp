96d8962f819e044cc3d5b9d70d97d1ff76c872d7
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-21 17:29:38 +0100

    Added first support for the automatic watering command.

--- src/rpi_gc/commands/automatic-watering/automatic-watering-command.cpp
+++ src/rpi_gc/commands/automatic-watering/automatic-watering-command.cpp
@@ -1,4 +1,27 @@
 // Copyright (C) 2022 Andrea Ballestrazzi
 #include <commands/automatic-watering/automatic-watering-command.hpp>
 
-namespace rpi_gc {} // namespace rpi_gc
+namespace rpi_gc {
+
+    AutomaticWateringCommand::AutomaticWateringCommand(ostream_ref outputStream, option_parser_pointer optionParser) noexcept :
+        m_outputStream{std::move(outputStream)},
+        m_optionParser{std::move(optionParser)} {}
+
+    bool AutomaticWateringCommand::processInputOptions(const std::vector<string_type>& inputTokens) noexcept {
+        assert(m_optionParser != nullptr);
+
+        m_optionParser->parse(inputTokens);
+        return true;
+    }
+
+    bool AutomaticWateringCommand::execute() noexcept {
+        return true;
+    }
+
+    void AutomaticWateringCommand::printHelp(help_ostream_type outputStream) const noexcept {
+        assert(m_optionParser != nullptr);
+
+        m_optionParser->printHelp(outputStream.get());
+    }
+
+} // namespace rpi_gc
--- src/rpi_gc/commands/automatic-watering/automatic-watering-command.hpp
+++ src/rpi_gc/commands/automatic-watering/automatic-watering-command.hpp
@@ -2,8 +2,41 @@
 #ifndef AUTOMATIC_WATERING_COMMAND_HPP
 #define AUTOMATIC_WATERING_COMMAND_HPP
 
+#include <user-interface/commands-strings.hpp>
 #include <commands/terminal-command.hpp>
+#include <gh_cmd/gh_cmd.hpp>
 
-namespace rpi_gc {} // namespace rpi_gc
+// C++ STL
+#include <functional>
+
+namespace rpi_gc {
+
+    //!!
+    //! \brief Represents the command that is executed when the user wants to activate or
+    //!  disable the automatic watering system.
+    class AutomaticWateringCommand : public TerminalCommandType {
+    public:
+        using option_parser = gh_cmd::OptionParser<char_type>;
+        using option_parser_pointer = std::unique_ptr<option_parser>;
+        using ostream_ref = std::reference_wrapper<std::basic_ostream<char_type>>;
+
+        AutomaticWateringCommand(ostream_ref outputStream, option_parser_pointer optionParser) noexcept;
+
+        bool processInputOptions(const std::vector<string_type>& inputTokens) noexcept override;
+
+        bool execute() noexcept override;
+
+        void printHelp(help_ostream_type outputStream) const noexcept override;
+
+        constexpr name_type getName() const noexcept override {
+            return strings::commands::AUTOMATIC_WATERING;
+        }
+
+    private:
+        ostream_ref m_outputStream;
+        option_parser_pointer m_optionParser{};
+    };
+
+} // namespace rpi_gc
 
 #endif // !AUTOMATIC_WATERING_COMMAND_HPP
