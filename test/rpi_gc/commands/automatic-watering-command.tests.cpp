beddd16ed309a5d3ea1336ea48a8992d10f478b2
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-11-23 17:35:59 +0100

    Fixed compilation error.

--- test/rpi_gc/commands/automatic-watering-command.tests.cpp
+++ test/rpi_gc/commands/automatic-watering-command.tests.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 Andrea Ballestrazzi
 #include <testing-core.hpp>
 
-#include <commands/automatic-watering-command.hpp>
+#include <commands/automatic-watering/automatic-watering-command.hpp>
 #include <user-interface/application-strings.hpp>
