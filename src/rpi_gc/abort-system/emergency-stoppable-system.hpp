ead8c979975dede967a85e12fdfdd00ee3fb1ea4
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-01 20:50:07 +0100

    Added emergency-stoppable-system.hpp header file.

---
+++ src/rpi_gc/abort-system/emergency-stoppable-system.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef EMERGENCY_STOPPABLE_SYSTEM
+#define EMERGENCY_STOPPABLE_SYSTEM
+
+namespace rpi_gc::abort_system {} // namespace rpi_gc
+
+#endif //! EMERGENCY_STOPPABLE_SYSTEM
