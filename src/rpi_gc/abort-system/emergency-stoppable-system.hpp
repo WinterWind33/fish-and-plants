b644f170353634d81207ff9fa6135cadfdc4c990
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-01 20:58:24 +0100

    Added EmergencyStoppableSystem class.

--- src/rpi_gc/abort-system/emergency-stoppable-system.hpp
+++ src/rpi_gc/abort-system/emergency-stoppable-system.hpp
@@ -2,6 +2,20 @@
 #ifndef EMERGENCY_STOPPABLE_SYSTEM
 #define EMERGENCY_STOPPABLE_SYSTEM
 
-namespace rpi_gc::abort_system {} // namespace rpi_gc
+namespace rpi_gc::abort_system {
+
+    //!!
+    //! \brief Represents the basic interface used to represent a system that is
+    //!  stoppable under an emergency.
+    class EmergencyStoppableSystem {
+    public:
+
+        //!!
+        //! \brief Notifies the system that an emergency is under way and
+        //!  it must shutdown immediately.
+        virtual void emergencyAbort() noexcept = 0;
+    };
+
+} // namespace rpi_gc
 
 #endif //! EMERGENCY_STOPPABLE_SYSTEM
