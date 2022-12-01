e5bd211f8d79a186d0b843b66233e8acafd9a5e4
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-01 21:33:53 +0100

    Added terminable-system.mock.hpp header file.

---
+++ test/rpi_gc/test-doubles/abort-system/terminable-system.mock.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef TERMINABLE_SYSTEM_MOCK_HPP
+#define TERMINABLE_SYSTEM_MOCK_HPP
+
+#include <abort-system/terminable-system.hpp>
+
+namespace rpi_gc::abort_system::mocks {} // namespace rpi_gc::abort_system::mocks
+
+#endif // !TERMINABLE_SYSTEM_MOCK_HPP
