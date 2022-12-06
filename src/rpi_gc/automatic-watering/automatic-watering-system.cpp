319514a5bee17f02c5691683836627715013e6ae
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-06 20:24:46 +0100

    Added logger support to AutomaticWateringSystem.

--- src/rpi_gc/automatic-watering/automatic-watering-system.cpp
+++ src/rpi_gc/automatic-watering/automatic-watering-system.cpp
@@ -1,6 +1,22 @@
 // Copyright (c) 2022 Andrea Ballestrazzi
 #include <automatic-watering/automatic-watering-system.hpp>
 
+// C++ STL
+#include <cassert>
+
 namespace rpi_gc::automatic_watering {
+
+    AutomaticWateringSystem::AutomaticWateringSystem(logger_pointer logger) noexcept :
+        m_logger{std::move(logger)} {
+        assert(m_logger != nullptr);
+    }
+
+    void AutomaticWateringSystem::requestShutdown() noexcept {
+        m_logger->logInfo("Shutdown requested for the automatic watering system.");
+    }
+
+    void AutomaticWateringSystem::emergencyAbort() noexcept {
+        m_logger->logWarning("[AUTOMATIC WATERING SYSTEM]: Emergency abort requested.");
+    }
 
 } // namespace rpi_gc::automatic_watering
--- src/rpi_gc/automatic-watering/automatic-watering-system.hpp
+++ src/rpi_gc/automatic-watering/automatic-watering-system.hpp
@@ -2,6 +2,31 @@
 #ifndef AUTOMATIC_WATERING_SYSTEM_HPP
 #define AUTOMATIC_WATERING_SYSTEM_HPP
 
-namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
+#include <abort-system/terminable-system.hpp>
+#include <abort-system/emergency-stoppable-system.hpp>
+
+#include <gh_log/logger.hpp>
+
+// C++ STL
+#include <memory>
+
+namespace rpi_gc::automatic_watering {
+
+class AutomaticWateringSystem : public abort_system::TerminableSystem, public abort_system::EmergencyStoppableSystem {
+public:
+    using logger_pointer = std::shared_ptr<gh_log::Logger>;
+
+    ~AutomaticWateringSystem() noexcept override = default;
+
+    AutomaticWateringSystem(logger_pointer logger) noexcept;
+
+    void requestShutdown() noexcept override;
+    void emergencyAbort() noexcept override;
+
+private:
+    logger_pointer m_logger{};
+};
+
+} // namespace rpi_gc::automatic_watering
 
 #endif // !AUTOMATIC_WATERING_SYSTEM_HPP
