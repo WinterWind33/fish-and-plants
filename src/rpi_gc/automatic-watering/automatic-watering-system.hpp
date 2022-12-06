d4daa2ee57c8e5b4493f83f6598689a9ea722017
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-06 18:49:18 +0100

    Added automatic-watering-system.hpp header file.

---
+++ src/rpi_gc/automatic-watering/automatic-watering-system.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef AUTOMATIC_WATERING_SYSTEM_HPP
+#define AUTOMATIC_WATERING_SYSTEM_HPP
+
+namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
+
+#endif // !AUTOMATIC_WATERING_SYSTEM_HPP
