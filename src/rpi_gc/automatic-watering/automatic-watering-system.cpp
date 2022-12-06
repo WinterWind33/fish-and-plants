cac99c537c5c50efc367008b39dea76a2ce51dac
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-06 19:01:46 +0100

    Added automatic-watering-system.cpp file.

---
+++ src/rpi_gc/automatic-watering/automatic-watering-system.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#include <automatic-watering/automatic-watering-system.hpp>
+
+namespace rpi_gc::automatic_watering {
+
+} // namespace rpi_gc::automatic_watering
