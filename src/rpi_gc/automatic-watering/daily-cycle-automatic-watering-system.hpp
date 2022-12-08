6e97dfa2f5ebc95956c710fe5caefe4a90b41e12
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-08 13:17:04 +0100

    Updated documentation.

--- src/rpi_gc/automatic-watering/daily-cycle-automatic-watering-system.hpp
+++ src/rpi_gc/automatic-watering/daily-cycle-automatic-watering-system.hpp
@@ -17,29 +17,48 @@
 
 namespace rpi_gc::automatic_watering {
 
+    //!!
+    //! \brief Represents the automatic watering system that manages the greenhouse
+    //!  irrigation on a daily basis using timers to stop and start the greenhouse's hardware.
     class DailyCycleAutomaticWateringSystem :
         public AutomaticWateringSystem,
         public abort_system::TerminableSystem,
         public abort_system::EmergencyStoppableSystem {
     public:
         using logger_pointer = std::shared_ptr<gh_log::Logger>;
+        using main_logger_pointer = logger_pointer;
+        using user_logger_pointer = logger_pointer;
         using thread_type = std::jthread;
 
         ~DailyCycleAutomaticWateringSystem() noexcept override = default;
 
-        DailyCycleAutomaticWateringSystem(logger_pointer mainLogger, logger_pointer userLog) noexcept;
+        //!!
+        //! \brief Construct a new Daily Cycle Automatic Watering System object using the specified
+        //!  main logger and user logger
+        //!
+        //! \param[in] mainLogger The logger that writes to the application main log file
+        //! \param[in] userLog The logger that prints the messages to the preferred user display device (std::cout mainly)
+        DailyCycleAutomaticWateringSystem(main_logger_pointer mainLogger, user_logger_pointer userLog) noexcept;
 
+        //!!
+        //! \brief Requests the automatic watering system to shutdown if the worker thread is running.
+        //!  If the worker thread is running, this calls wait for its completion.
+        //! \note Possibly a blocking call.
         void requestShutdown() noexcept override;
+
+        //!!
+        //! \brief (FOR NOW) Same as requestShutdown.
+        //! \note Possibly a blocking call.
         void emergencyAbort() noexcept override;
 
         void startAutomaticWatering() noexcept override;
 
     private:
-        logger_pointer m_mainLogger{};
-        logger_pointer m_userLogger{};
+        main_logger_pointer m_mainLogger{};
+        user_logger_pointer m_userLogger{};
         thread_type m_workerThread{};
 
-        void run_automatic_watering(std::stop_token stopToken, logger_pointer logger) noexcept;
+        void run_automatic_watering(std::stop_token stopToken, main_logger_pointer logger) noexcept;
 
         [[nodiscard]] static StringType format_log_string(StringViewType message) noexcept;
     };
