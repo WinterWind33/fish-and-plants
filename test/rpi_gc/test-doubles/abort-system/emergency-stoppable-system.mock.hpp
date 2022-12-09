97c8a3ac981368f42a069e1e3cc573c8e34ee234
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-09 13:06:21 +0100

    Added EmergencyStoppableSystemMock class.

--- test/rpi_gc/test-doubles/abort-system/emergency-stoppable-system.mock.hpp
+++ test/rpi_gc/test-doubles/abort-system/emergency-stoppable-system.mock.hpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2022 Andrea Ballestrazzi
-#ifndef EMERGENCY_STOPPABLE_SYSTEM_HPP
-#define EMERGENCY_STOPPABLE_SYSTEM_HPP
+#ifndef EMERGENCY_STOPPABLE_SYSTEM_MOCK_HPP
+#define EMERGENCY_STOPPABLE_SYSTEM_MOCK_HPP
 
 #include <abort-system/emergency-stoppable-system.hpp>
 
@@ -8,6 +8,15 @@
 #include <gmock/gmock.h>
 #endif // USE_GMOCK
 
-namespace rpi_gc::abort_system::mocks {} // namespace rpi_gc::abort_system::mocks
+namespace rpi_gc::abort_system::mocks {
 
-#endif // !EMERGENCY_STOPPABLE_SYSTEM_HPP
+    class EmergencyStoppableSystemMock : public rpi_gc::abort_system::EmergencyStoppableSystem {
+    public:
+        ~EmergencyStoppableSystemMock() noexcept override = default;
+
+        MOCK_METHOD(void, emergencyAbort, (), (noexcept, override));
+    };
+
+} // namespace rpi_gc::abort_system::mocks
+
+#endif // !EMERGENCY_STOPPABLE_SYSTEM_MOCK_HPP
