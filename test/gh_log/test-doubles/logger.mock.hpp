a894a2242957f98d8f4c8eb41c7be8b1fb491936
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-09 12:31:36 +0100

    Added LoggerMock implementation.

--- test/gh_log/test-doubles/logger.mock.hpp
+++ test/gh_log/test-doubles/logger.mock.hpp
@@ -2,6 +2,25 @@
 #ifndef LOGGER_MOCK_HPP
 #define LOGGER_MOCK_HPP
 
-namespace gh_log::mocks {} // namespace gh_log::mocks
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif // USE_GMOCK
+
+#include <gh_log/logger.hpp>
+
+namespace gh_log::mocks {
+
+    class LoggerMock : public gh_log::Logger {
+    public:
+        ~LoggerMock() noexcept override = default;
+
+        MOCK_METHOD(void, logTrace, (const LogStringType&), (override));
+        MOCK_METHOD(void, logDebug, (const LogStringType&), (override));
+        MOCK_METHOD(void, logInfo, (const LogStringType&), (override));
+        MOCK_METHOD(void, logWarning, (const LogStringType&), (override));
+        MOCK_METHOD(void, logCritical, (const LogStringType&), (override));
+    };
+
+} // namespace gh_log::mocks
 
 #endif // !LOGGER_MOCK_HPP
