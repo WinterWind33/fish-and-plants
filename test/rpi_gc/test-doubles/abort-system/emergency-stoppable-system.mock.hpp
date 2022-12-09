b91ec55037fe46482a4403aa971539146536dea2
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2022-12-09 13:03:40 +0100

    Added emergency-stoppable-system.mock.hpp header file.

---
+++ test/rpi_gc/test-doubles/abort-system/emergency-stoppable-system.mock.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Andrea Ballestrazzi
+#ifndef EMERGENCY_STOPPABLE_SYSTEM_HPP
+#define EMERGENCY_STOPPABLE_SYSTEM_HPP
+
+#include <abort-system/emergency-stoppable-system.hpp>
+
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif // USE_GMOCK
+
+namespace rpi_gc::abort_system::mocks {} // namespace rpi_gc::abort_system::mocks
+
+#endif // !EMERGENCY_STOPPABLE_SYSTEM_HPP
