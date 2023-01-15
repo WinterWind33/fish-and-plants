cc7064172121790957208b16c817d3ebc8003ea8
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-15 15:00:18 +0100

    Added watering-system-hardware-controller.hpp header file.

---
+++ src/rpi_gc/automatic-watering/hardware-controllers/watering-system-hardware-controller.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#ifndef WATERING_SYSTEM_HARDWARE_CONTROLLER
+#define WATERING_SYSTEM_HARDWARE_CONTROLLER
+
+namespace rpi_gc::automatic_watering {} // namespace std::automatic_watering
+
+#endif // !WATERING_SYSTEM_HARDWARE_CONTROLLER
