251d7da0b339e9a60058ba0d66602c7eaa0f3dd8
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-15 15:13:51 +0100

    Added daily-cycle-aws-hardware-controller .hpp and .cpp files.

---
+++ src/rpi_gc/automatic-watering/hardware-controllers/daily-cycle-aws-hardware-controller.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+
+#include <automatic-watering/hardware-controllers/daily-cycle-aws-hardware-controller.hpp>
+
+namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
---
+++ src/rpi_gc/automatic-watering/hardware-controllers/daily-cycle-aws-hardware-controller.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#ifndef DAILY_CYCLE_AWS_HARDWARE_CONTROLLER_HPP
+#define DAILY_CYCLE_AWS_HARDWARE_CONTROLLER_HPP
+
+#include <automatic-watering/hardware-controllers/watering-system-hardware-controller.hpp>
+
+namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
+
+#endif // !DAILY_CYCLE_AWS_HARDWARE_CONTROLLER_HPP
