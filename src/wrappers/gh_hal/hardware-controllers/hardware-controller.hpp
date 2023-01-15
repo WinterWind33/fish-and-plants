76efa32dd2b0d09fa6b34d15117386e7f683435c
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-15 14:56:01 +0100

    Revert "Added hardware-controller.hpp header file."
    This reverts commit e13cc3ed68fbe8b15d4a7a0bcb8543f0eee2b422.

--- src/wrappers/gh_hal/hardware-controllers/hardware-controller.hpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2023 Andrea Ballestrazzi
-#ifndef HARDWARE_CONTROLLER_HPP
-#define HARDWARE_CONTROLLER_HPP
-
-namespace gh_hal {} // namespace gh_hal
-
-#endif // !HARDWARE_CONTROLLER_HPP
