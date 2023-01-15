e13cc3ed68fbe8b15d4a7a0bcb8543f0eee2b422
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-15 14:54:09 +0100

    Added hardware-controller.hpp header file.

---
+++ src/wrappers/gh_hal/hardware-controllers/hardware-controller.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#ifndef HARDWARE_CONTROLLER_HPP
+#define HARDWARE_CONTROLLER_HPP
+
+namespace gh_hal {} // namespace gh_hal
+
+#endif // !HARDWARE_CONTROLLER_HPP
