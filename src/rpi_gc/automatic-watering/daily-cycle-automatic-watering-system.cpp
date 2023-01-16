ba5b53595121db44b6b7c5b01d161ea7d715fa9e
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-16 18:47:09 +0100

    Fixed copyright notices and added time-provider header file for the automatic watering system.

--- src/rpi_gc/automatic-watering/daily-cycle-automatic-watering-system.cpp
+++ src/rpi_gc/automatic-watering/daily-cycle-automatic-watering-system.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 Andrea Ballestrazzi
+// Copyright (c) 2023 Andrea Ballestrazzi
 #include <automatic-watering/daily-cycle-automatic-watering-system.hpp>
 
 #include <common/types.hpp>
---
+++ src/rpi_gc/automatic-watering/time-providers/watering-system-time-provider.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#ifndef WATERING_SYSTEM_TIME_PROVIDER_HPP
+#define WATERING_SYSTEM_TIME_PROVIDER_HPP
+
+namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
+
+#endif // !WATERING_SYSTEM_TIME_PROVIDER_HPP
