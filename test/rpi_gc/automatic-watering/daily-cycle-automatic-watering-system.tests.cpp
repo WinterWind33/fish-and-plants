8c51915f5dbf0ee2a79c033e582007d48e039c98
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-16 21:55:24 +0100

    Added includes to test file.

--- test/rpi_gc/automatic-watering/daily-cycle-automatic-watering-system.tests.cpp
+++ test/rpi_gc/automatic-watering/daily-cycle-automatic-watering-system.tests.cpp
@@ -1,1 +1,14 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+
+#include <automatic-watering/daily-cycle-automatic-watering-system.hpp>
+
+// Test Doubles
+#include <rpi_gc/test-doubles/automatic-watering/time-providers/watering-system-time-provider.mock.hpp>
+#include <rpi_gc/test-doubles/automatic-watering/hardware-controllers/watering-system-hardware-controller.mock.hpp>
+#include <gh_log/test-doubles/logger.mock.hpp>
+
+#include <testing-core.hpp>
+
+TEST_CASE("DailyCycleAutomaticWateringSystem unit tests", "[unit][solitary][rpi_gc][automatic-watering][DailyCycleAutomaticWateringSystem]") {
+
+}
