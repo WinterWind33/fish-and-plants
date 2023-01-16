dd8fd6f3107b819fcfbe2fa78fd36b9e96ebdfd6
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-16 21:30:11 +0100

    Added WateringSystemHardwareControllerMock class.

--- test/rpi_gc/test-doubles/automatic-watering/hardware-controllers/watering-system-hardware-controller.mock.hpp
+++ test/rpi_gc/test-doubles/automatic-watering/hardware-controllers/watering-system-hardware-controller.mock.hpp
@@ -2,6 +2,20 @@
 #ifndef WATERING_SYSTEM_HARDWARE_CONTROLLER_MOCK_HPP
 #define WATERING_SYSTEM_HARDWARE_CONTROLLER_MOCK_HPP
 
-namespace rpi_gc::automatic_watering::mocks {} // namespace rpi_gc::automatic_watering::mocks
+#include <automatic-watering/hardware-controllers/watering-system-hardware-controller.hpp>
+
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif // USE_GMOCK
+
+namespace rpi_gc::automatic_watering::mocks {
+
+    class WateringSystemHardwareControllerMock : public WateringSystemHardwareController {
+    public:
+        MOCK_METHOD(digital_output_type*, getWaterValveDigitalOut, (), (noexcept, override));
+        MOCK_METHOD(digital_output_type*, getWaterPumpDigitalOut, (), (noexcept, override));
+    };
+
+} // namespace rpi_gc::automatic_watering::mocks
 
 #endif // !WATERING_SYSTEM_HARDWARE_CONTROLLER_MOCK_HPP
