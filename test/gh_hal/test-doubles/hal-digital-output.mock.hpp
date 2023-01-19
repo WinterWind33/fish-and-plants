ee047c1ccd48a98acbba3de650917de7f12a66f5
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-19 17:39:25 +0100

    Added HALDigitalOutputMock class.

--- test/gh_hal/test-doubles/hal-digital-output.mock.hpp
+++ test/gh_hal/test-doubles/hal-digital-output.mock.hpp
@@ -2,10 +2,20 @@
 #ifndef HAL_DIGITAL_OUTPUT_MOCK_HPP
 #define HAL_DIGITAL_OUTPUT_MOCK_HPP
 
+#include <gh_hal/hal-digital-output.hpp>
+
 #ifdef USE_GMOCK
 #include <gmock/gmock.h>
 #endif // USE_GMOCK
 
-namespace gh_hal::mocks {} // namespace gh_hal::mocks
+namespace gh_hal::mocks {
+
+    class HALDigitalOutputMock : public HALDigitalOutput {
+    public:
+        MOCK_METHOD(void, turnOn, (), (noexcept, override));
+        MOCK_METHOD(void, turnOff, (), (noexcept, override));
+    };
+
+} // namespace gh_hal::mocks
 
 #endif // !HAL_DIGITAL_OUTPUT_MOCK_HPP
