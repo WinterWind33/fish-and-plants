36c1243a4a13d9d8dfc8361d04c2e2d399b58115
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-19 17:36:11 +0100

    Added hal-digital-output.mock.hpp header file.

---
+++ test/gh_hal/test-doubles/hal-digital-output.mock.hpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#ifndef HAL_DIGITAL_OUTPUT_MOCK_HPP
+#define HAL_DIGITAL_OUTPUT_MOCK_HPP
+
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif // USE_GMOCK
+
+namespace gh_hal::mocks {} // namespace gh_hal::mocks
+
+#endif // !HAL_DIGITAL_OUTPUT_MOCK_HPP
