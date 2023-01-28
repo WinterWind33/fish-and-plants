ef7d2eaceb77a47d65d2d06470afa76f1c7eb2fc
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-28 16:33:59 +0100

    Added configurable time provider implementation.

--- src/rpi_gc/automatic-watering/time-providers/configurable-daily-cycle-aws-time-provider.cpp
+++ src/rpi_gc/automatic-watering/time-providers/configurable-daily-cycle-aws-time-provider.cpp
@@ -1,4 +1,39 @@
 // Copyright (c) Andrea Ballestrazzi
 #include <automatic-watering/time-providers/configurable-daily-cycle-aws-time-provider.hpp>
 
-namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
+namespace rpi_gc::automatic_watering {
+
+    ConfigurableDailyCycleAWSTimeProvider::ConfigurableDailyCycleAWSTimeProvider(
+            const time_unit initialActivationTime,
+            const time_unit initialDeactivationTime,
+            const time_unit initialPumpValveWaitTime) noexcept :
+            m_activationTimeTicks{initialActivationTime.count()},
+            m_deactivationTimeTicks{initialDeactivationTime.count()},
+            m_pumpValveWaitTimeTicks{initialPumpValveWaitTime.count()} {}
+
+    auto ConfigurableDailyCycleAWSTimeProvider::getWateringSystemActivationDuration() const noexcept -> time_unit {
+        return time_unit{m_activationTimeTicks.load()};
+    }
+
+    auto ConfigurableDailyCycleAWSTimeProvider::getWateringSystemDeactivationDuration() const noexcept -> time_unit {
+        return time_unit{m_deactivationTimeTicks.load()};
+    }
+
+    auto ConfigurableDailyCycleAWSTimeProvider::getPumpValveDeactivationTimeSeparation() const noexcept -> time_unit {
+        return time_unit{m_pumpValveWaitTimeTicks.load()};
+    }
+
+    void ConfigurableDailyCycleAWSTimeProvider::setActivationTimeTicks(const rep_type ticks) noexcept {
+        m_activationTimeTicks.store(ticks);
+    }
+
+    void ConfigurableDailyCycleAWSTimeProvider::setDeactivationTimeTicks(const rep_type ticks) noexcept {
+        m_deactivationTimeTicks.store(ticks);
+    }
+
+    void ConfigurableDailyCycleAWSTimeProvider::setPumpValveWaitTimeTicks(const rep_type ticks) noexcept {
+        m_pumpValveWaitTimeTicks.store(ticks);
+    }
+
+
+} // namespace rpi_gc::automatic_watering
