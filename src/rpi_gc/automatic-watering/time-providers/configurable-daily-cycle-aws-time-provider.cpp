daa25d6440868779e8d10a0bb4b2522153b18b3f
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-01-28 16:21:25 +0100

    Added initial class and cpp file.

---
+++ src/rpi_gc/automatic-watering/time-providers/configurable-daily-cycle-aws-time-provider.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) Andrea Ballestrazzi
+#include <automatic-watering/time-providers/configurable-daily-cycle-aws-time-provider.hpp>
+
+namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
--- src/rpi_gc/automatic-watering/time-providers/configurable-daily-cycle-aws-time-provider.hpp
+++ src/rpi_gc/automatic-watering/time-providers/configurable-daily-cycle-aws-time-provider.hpp
@@ -2,6 +2,30 @@
 #ifndef CONFIGURABLE_DAILY_CYCLE_AWS_TIME_PROVIDER_HPP
 #define CONFIGURABLE_DAILY_CYCLE_AWS_TIME_PROVIDER_HPP
 
-namespace rpi_gc::automatic_watering {} // namespace rpi_gc::automatic_watering
+#include <automatic-watering/time-providers/watering-system-time-provider.hpp>
+
+// C++ STL
+#include <cstdint>
+#include <atomic>
+
+namespace rpi_gc::automatic_watering {
+
+    class ConfigurableDailyCycleAWSTimeProvider : public WateringSystemTimeProvider {
+    public:
+        using rep_type = time_unit::rep;
+
+        ConfigurableDailyCycleAWSTimeProvider(
+            const time_unit initialActivationTime,
+            const time_unit initialDeactivationTime,
+            const time_unit initialPumpValveWaitTime) noexcept;
+
+
+    private:
+        std::atomic<rep_type> m_activationTimeTicks{};
+        std::atomic<rep_type> m_deactivationTimeTicks{};
+        std::atomic<rep_type> m_pumpValveWaitTimeTicks{};
+    };
+
+} // namespace rpi_gc::automatic_watering
 
 #endif // !CONFIGURABLE_DAILY_CYCLE_AWS_TIME_PROVIDER_HPP
