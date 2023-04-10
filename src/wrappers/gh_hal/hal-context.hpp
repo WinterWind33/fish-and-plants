e73b70ae7bbe4d006d8dbe4c07f4c93279a3a9de
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-10 21:02:18 +0200

    Added simple initialization for ligpiod chip.

--- src/wrappers/gh_hal/hal-context.hpp
+++ src/wrappers/gh_hal/hal-context.hpp
@@ -4,19 +4,26 @@
 
 #include <gh_log/logger.hpp>
 
+#ifdef USE_LIBGPIOD
+#include <gpiod.h>
+#endif // USE_LIBGPIOD
+
 // C++ STL
 #include <memory>
 
-namespace gh_hal {
+namespace gh_hal
+{
 
-    class HALContext {
+    class HALContext
+    {
     public:
         using logger_pointer = std::shared_ptr<gh_log::Logger>;
 
         HALContext(logger_pointer logger, const bool bIsSim = true, const bool bForceFullMap = false) noexcept;
-        virtual ~HALContext() noexcept = default;
+        virtual ~HALContext() noexcept;
 
-        virtual const logger_pointer& getLogger() const noexcept {
+        virtual const logger_pointer &getLogger() const noexcept
+        {
             return m_logger;
         }
 
@@ -25,6 +32,12 @@ namespace gh_hal {
     private:
         logger_pointer m_logger{};
         bool m_bIsSimulation{};
+
+#ifdef USE_LIBGPIOD
+        using chip_type = struct gpiod_chip;
+
+        chip_type *m_boardChip{};
+#endif // USE_LIBGPIOD
     };
 
 } // namespace gh_hal
