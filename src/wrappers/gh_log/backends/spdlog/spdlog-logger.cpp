8082eb5673bfeeddf1501647334518cbb9fbf133
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-13 21:50:03 +0200

    Added spdlog-logger.cpp file.

---
+++ src/wrappers/gh_log/backends/spdlog/spdlog-logger.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#include <gh_log/backends/spdlog/spdlog-logger.hpp>
+
+namespace gh_log::backends::spdlog_impl {
+
+} // namespace gh_log::backends::spdlog_impl
