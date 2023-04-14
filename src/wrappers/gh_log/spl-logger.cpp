1659058148591d8b10925f520cb8d02b7906493e
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-14 14:53:35 +0200

    Added implementation to SPLLogger.

--- src/wrappers/gh_log/spl-logger.cpp
+++ src/wrappers/gh_log/spl-logger.cpp
@@ -9,6 +9,46 @@
 
 namespace gh_log {
 
+    namespace details {
+
+        struct LoggingLevelConverter {
+            using spdlog_logging_level = spdlog::level::level_enum;
+            using map_type = std::array<std::pair<ELoggingLevel, spdlog_logging_level>, spdlog_logging_level::n_levels>;
+
+            static constexpr map_type ToSpdlogMap{
+                {
+                    {ELoggingLevel::Trace, spdlog_logging_level::trace},
+                    {ELoggingLevel::Debug, spdlog_logging_level::debug},
+                    {ELoggingLevel::Info, spdlog_logging_level::info},
+                    {ELoggingLevel::Warning, spdlog_logging_level::warn},
+                    {ELoggingLevel::Error, spdlog_logging_level::err},
+                    {ELoggingLevel::Critical, spdlog_logging_level::critical}
+                }
+            };
+
+            [[nodiscard]]
+            static constexpr spdlog_logging_level toSpdlogLevel(const ELoggingLevel level) {
+                const auto iter{std::find_if(std::begin(ToSpdlogMap), std::end(ToSpdlogMap), [level](const auto& val){
+                    return std::get<0>(val) == level;
+                })};
+
+                if (iter != std::end(ToSpdlogMap)) {
+                    return std::get<1>(*iter);
+                } else {
+                    throw std::range_error("SPDLOG Logging level not found!");
+                }
+            }
+        };
+
+        static_assert(LoggingLevelConverter::toSpdlogLevel(ELoggingLevel::Trace) == LoggingLevelConverter::spdlog_logging_level::trace);
+        static_assert(LoggingLevelConverter::toSpdlogLevel(ELoggingLevel::Debug) == LoggingLevelConverter::spdlog_logging_level::debug);
+        static_assert(LoggingLevelConverter::toSpdlogLevel(ELoggingLevel::Info) == LoggingLevelConverter::spdlog_logging_level::info);
+        static_assert(LoggingLevelConverter::toSpdlogLevel(ELoggingLevel::Warning) == LoggingLevelConverter::spdlog_logging_level::warn);
+        static_assert(LoggingLevelConverter::toSpdlogLevel(ELoggingLevel::Error) == LoggingLevelConverter::spdlog_logging_level::err);
+        static_assert(LoggingLevelConverter::toSpdlogLevel(ELoggingLevel::Critical) == LoggingLevelConverter::spdlog_logging_level::critical);
+
+    } // namespace details
+
     SPLLogger::SPLLogger(logger_pointer logger) :
         m_logger{std::move(logger)} {
         assert(m_logger);
@@ -51,7 +91,7 @@ namespace gh_log {
     }
 
     void SPLLogger::logMessage(const ELoggingLevel level, LogStringType message) {
-
+        m_logger->log(details::LoggingLevelConverter::toSpdlogLevel(level), message);
     }
 
     void SPLLogger::flush() {
@@ -59,7 +99,7 @@ namespace gh_log {
     }
 
     void SPLLogger::setAutomaticFlushLevel(const ELoggingLevel level) {
-
+        m_logger->flush_on(details::LoggingLevelConverter::toSpdlogLevel(level));
     }
 
     std::shared_ptr<SPLLogger> SPLLogger::createFileLogger(std::string name, std::filesystem::path filepath) noexcept {
