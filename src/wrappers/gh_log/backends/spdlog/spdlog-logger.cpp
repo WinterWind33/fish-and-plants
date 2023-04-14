1f60c7c09ea554e7097e6ff04dea8f9bc0ca6881
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-14 14:40:18 +0200

    Removed useless code.

--- src/wrappers/gh_log/backends/spdlog/spdlog-logger.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2023 Andrea Ballestrazzi
-#include <gh_log/backends/spdlog/spdlog-logger.hpp>
-
-namespace gh_log::backends::spdlog_impl {
-
-} // namespace gh_log::backends::spdlog_impl
--- src/wrappers/gh_log/backends/spdlog/spdlog-logger.hpp
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2023 Andrea Ballestrazzi
-#pragma once
-
-namespace gh_log::backends::spdlog_impl {} // namespace gh_log::backends::spdlog_impl
