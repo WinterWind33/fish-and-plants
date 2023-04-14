194c8f4e21e48a0d786cb00cb32d12c0d8de9681
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-14 22:52:32 +0200

    Added miscellaneous.cpp file.

---
+++ src/wrappers/gh_hal/backends/libgpiod/miscellaneous.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#include <gh_hal/backends/libgpiod/miscellaneous.hpp>
+
+namespace gh_hal::backends::libgpiod_impl {} // namespace gh_hal::backends::libgpiod_impl
