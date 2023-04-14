413787a85094d434e81420ebecbd739bb2019e45
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-14 21:51:32 +0200

    Added BackendModule class implementation.

--- src/wrappers/gh_hal/hardware-abstraction-layer.hpp
+++ src/wrappers/gh_hal/hardware-abstraction-layer.hpp
@@ -1,2 +1,30 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
+
+// C++ STL
+#include <string>
+#include <cstdint>
+
+namespace gh_hal {
+
+    //!!
+    //! \brief Utility class, used to retrieve info about the
+    //! backend module.
+    //!
+    class BackendModule final {
+    public:
+        using name_type = std::string;
+        using version_type = std::string;
+
+        //!!
+        //! \brief Get the Backend Module Name
+        //!
+        static name_type GetBackendModuleName() noexcept;
+
+        //!!
+        //! \brief Get the Backend Module Version
+        //!
+        static version_type GetBackendModuleVersion() noexcept;
+    };
+
+} // namespace gh_hal
