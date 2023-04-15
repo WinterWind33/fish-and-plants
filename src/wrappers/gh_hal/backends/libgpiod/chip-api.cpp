2184544ad3afdafa9a26765e326b63837f3391a1
WinterWind33 <ballestrazziandrea@outlook.com>
2023-04-15 09:45:01 +0200

    Added chip-api.cpp file.

---
+++ src/wrappers/gh_hal/backends/libgpiod/chip-api.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#include <gh_hal/backends/libgpiod/chip-api.hpp>
+
+namespace gh_log::backends::libgpiod_impl {
+
+} // namespace gh_hal::backends::libgpiod_impl
