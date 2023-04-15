6b7e447fa38a6cae1c8366a8378a8d94cbb6c577
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-15 14:49:38 +0200

    Added simulated-chip.cpp file.

---
+++ src/wrappers/gh_hal/backends/simulated/simulated-chip.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#include <gh_hal/backends/simulated/simulated-chip.hpp>
+
+namespace gh_hal::backends::simulated {} // namespace gh_hal::backends::simulated
