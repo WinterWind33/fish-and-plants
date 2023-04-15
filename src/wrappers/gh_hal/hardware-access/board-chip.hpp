4c4a58d4a16cff7d429b62ce1f265298653059d5
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-15 15:34:35 +0200

    Added board chip factory and improved documentation.

--- src/wrappers/gh_hal/hardware-access/board-chip.hpp
+++ src/wrappers/gh_hal/hardware-access/board-chip.hpp
@@ -7,8 +7,27 @@
 
 namespace gh_hal::hardware_access {
 
+    //!!
+    //! \brief Basic interface of a board chip that can be used to
+    //!  access the hardware and it can provide access to the board's PINs.
     struct BoardChip {
         virtual ~BoardChip() noexcept = default;
+    };
+
+    //!!
+    //! \brief Represents the standard factory that can open chips.
+    //!
+    class BoardChipFactory final {
+    public:
+
+        //!!
+        //! \brief Opens a chip that is represented by the given path, usually something
+        //!  like "/dev/gpiochip0".
+        //!  This can throw an exception if the chip cannot be opened.
+        //!
+        //! \param chipPath The path of the chip to be opened.
+        //! \return A pointer to the opened chip or nullptr in case of an error.
+        static std::unique_ptr<BoardChip> openChipByPath(std::filesystem::path chipPath);
     };
 
 } // namespace hardware_access
