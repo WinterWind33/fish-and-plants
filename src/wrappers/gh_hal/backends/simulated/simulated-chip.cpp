74aa6afdbd8572bc49d70572741f90c6be3954bd
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-15 15:09:51 +0200

    Added SimulatedChip class.

--- src/wrappers/gh_hal/backends/simulated/simulated-chip.cpp
+++ src/wrappers/gh_hal/backends/simulated/simulated-chip.cpp
@@ -1,4 +1,17 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #include <gh_hal/backends/simulated/simulated-chip.hpp>
 
-namespace gh_hal::backends::simulated {} // namespace gh_hal::backends::simulated
+// C++ STL
+
+namespace gh_hal::backends::simulated {
+
+    SimulatedChip::SimulatedChip(chip_path chipPath) noexcept :
+        m_chipPath{std::move(chipPath)},
+        m_bIsOpened{true} {}
+
+    void SimulatedChip::close() noexcept {
+        m_bIsOpened = false;
+        m_chipPath.clear();
+    }
+
+} // namespace gh_hal::backends::simulated
