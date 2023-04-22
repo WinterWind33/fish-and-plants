650c12032a12abd5a0f1459fe85bd2abced9ae94
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-22 18:21:29 +0200

    Added simulated line request to the board chip implementation class.

--- src/wrappers/gh_hal/internal/board-chip-impl.cpp
+++ src/wrappers/gh_hal/internal/board-chip-impl.cpp
@@ -1,12 +1,16 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #include <gh_hal/internal/board-chip-impl.hpp>
+#include <gh_hal/internal/board-digital-pin-impl.hpp>
 
 #ifdef USE_LIBGPIOD
 #include <gh_hal/backends/libgpiod/chip-api.hpp>
+#else
+#include <gh_hal/backends/simulated/simulated-digital-board-pin.hpp>
 #endif // USE_LIBGPIOD
 
 // C++ STL
 #include <cassert>
+#include <vector>
 #include <array>
 #include <tuple>
 #include <algorithm> // for std::find_if
@@ -57,8 +61,43 @@ namespace gh_hal::internal {
         }
 
 #else
-        std::unique_ptr<BackendChipType> openChip(std::filesystem::path chipPath) {
+        [[nodiscard]] std::unique_ptr<BackendChipType> openChip(std::filesystem::path chipPath) {
             return std::make_unique<backends::simulated::SimulatedChip>(std::move(chipPath));
+        }
+
+        using FakeLineRequest = std::pair<hardware_access::DigitalPinRequestDirection, std::vector<simulated::DigitalBoardPin>>;
+
+        [[nodiscard]]
+        FakeLineRequest requestLines(BackendChipType& chip,
+            [[maybe_unused]] std::string /*consumer*/,
+            std::vector<hardware_access::BoardDigitalPin::offset_type> offsets,
+            [[maybe_unused]] hardware_access::DigitalPinRequestDirection direction) noexcept {
+
+            FakeLineRequest lineRequest{};
+            std::get<0>(lineRequest) = direction;
+
+            std::transform(offsets.cbegin(), offsets.cend(), std::back_inserter(std::get<1>(lineRequest)),
+                [](const hardware_access::BoardDigitalPin::offset_type offset){
+                return simulated::DigitalBoardPin{offset};
+            });
+
+            return lineRequest;
+        }
+
+        [[nodiscard]]
+        std::vector<std::unique_ptr<hardware_access::BoardDigitalPin>> createDigitalPinsFromLineRequest(const FakeLineRequest& lineRequest) noexcept {
+            using resulting_vector_type = std::vector<std::unique_ptr<hardware_access::BoardDigitalPin>>;
+
+            resulting_vector_type resultVector{};
+            const auto requestDirection{std::get<0>(lineRequest)};
+            const auto& simulatedPins{std::get<1>(lineRequest)};
+
+            std::transform(simulatedPins.cbegin(), simulatedPins.cend(), std::back_inserter(resultVector),
+                [requestDirection](const simulated::DigitalBoardPin& boardPin){
+                return std::make_unique<internal::BoardDigitalPinImpl>(boardPin.getOffsetValue(), requestDirection, boardPin);
+            });
+
+            return resultVector;
         }
 #endif // USE_LIBGPIOD
     } // namespace details
@@ -85,12 +124,26 @@ namespace gh_hal::internal {
             direction
         );
 
-        return boardDigitalPin;
+        auto boardPins = details::createDigitalPinsFromLineRequest(lineRequest);
+        assert(boardPins.size() == 1);
+
+        // We firstly need to move the ownership from vector to a local variable because
+        // otherwise we will lose RVO.
+        auto boardPin{std::move(boardPins[0])};
+        return boardPin;
     }
 
     std::vector<std::unique_ptr<hardware_access::BoardDigitalPin>> BoardChipImpl::requestDigitalPinPool(std::string consumer,
             std::vector<hardware_access::BoardDigitalPin::offset_type> offset, const hardware_access::DigitalPinRequestDirection direction) noexcept {
-        return {};
+        assert(static_cast<bool>(*m_chipPtr));
+
+        auto lineRequest = details::requestLines(*m_chipPtr,
+            std::move(consumer),
+            {offset},
+            direction
+        );
+
+        return details::createDigitalPinsFromLineRequest(lineRequest);
     }
 
 } // namespace gh_hal::internal
