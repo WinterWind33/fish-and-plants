46a8336c976eb8ffe090a15989f5b575820263b1
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-22 11:26:29 +0200

    Added board-chip.mock.hpp header file.

---
+++ test/gh_hal/test-doubles/hardware-access/board-chip.mock.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#pragma once
+
+#include <gh_hal/hardware-access/board-chip.hpp>
+
+namespace gh_hal::hardware_access::mocks {
+
+    class BoardChipMock : public BoardChip {
+    public:
+    };
+
+} // namespace gh_hal::hardware_access::mocks
