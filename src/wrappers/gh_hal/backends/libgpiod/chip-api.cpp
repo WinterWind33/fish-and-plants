86529440e963f83dc60c9eca5049b57879b48542
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-22 13:07:02 +0200

    Added requestLines() function.

--- src/wrappers/gh_hal/backends/libgpiod/chip-api.cpp
+++ src/wrappers/gh_hal/backends/libgpiod/chip-api.cpp
@@ -1,10 +1,42 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #include <gh_hal/backends/libgpiod/chip-api.hpp>
 
+// C++ STL
+#include <algorithm> // for std::transform
+#include <cassert>
+
 namespace gh_hal::backends::libgpiod_impl {
 
-    std::unique_ptr<gpiod::chip> openChip(std::filesystem::path chipPath) {
-        return std::make_unique<gpiod::chip>(chipPath);
+    std::unique_ptr<NativeChipType> openChip(std::filesystem::path chipPath) {
+        return std::make_unique<NativeChipType>(chipPath);
+    }
+
+    NativeLineRequestType requestLines(NativeChipType& chip, std::string consumer, std::vector<NativeLineOffsetType> offsets,
+        const NativeLineDirectionType direction) {
+        assert(static_cast<bool>(chip));
+
+        ::gpiod::line::offsets nativeOffsets{};
+
+        // Now we need to transform the input offsets vector to a gpiod vector
+        // because libgpiod stores offsets in a custom class that accepts the value
+        // as its only parameter.
+        std::transform(offsets.cbegin(), offsets.cend(), std::back_inserter(nativeOffsets), [](const NativeLineOffsetType offsetValue){
+            return ::gpiod::line::offset{offsetValue};
+        });
+
+        // Now we can perform the native request.
+        NativeLineRequestType lineRequest{
+            chip.prepare_request()
+                .set_consumer(consumer)
+                .add_line_settings(
+                    nativeOffsets,
+                    ::gpiod::line_settings{}
+                        .set_direction(direction)
+                )
+                .do_request()
+        };
+
+        return lineRequest;
     }
 
 } // namespace gh_hal::backends::libgpiod_impl
--- src/wrappers/gh_hal/backends/libgpiod/chip-api.hpp
+++ src/wrappers/gh_hal/backends/libgpiod/chip-api.hpp
@@ -4,11 +4,26 @@
 #include <gpiod.hpp>
 
 // C++ STL
+#include <string>
+#include <vector>
 #include <filesystem>
 #include <memory>
+#include <cstdint>
 
 namespace gh_hal::backends::libgpiod_impl {
 
-    std::unique_ptr<gpiod::chip> openChip(std::filesystem::path chipPath);
+    using NativeChipType = ::gpiod::chip;
+    using NativeLineRequestType = ::gpiod::line_request;
+    using NativeLineDirectionType = ::gpiod::line::direction;
+    using NativeLineOffsetType = std::uint32_t;
+
+    std::unique_ptr<NativeChipType> openChip(std::filesystem::path chipPath);
+
+    NativeLineRequestType requestLines(
+        NativeChipType& chip,
+        std::string consumer,
+        std::vector<NativeLineOffsetType> offsets,
+        const NativeLineDirectionType direction
+    );
 
 } // namespace gh_hal::backends::libgpiod_impl
