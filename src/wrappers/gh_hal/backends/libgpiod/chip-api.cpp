3ee521cffe5c2b2bf5d8d788ca639f457dd6e4d0
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-22 12:34:16 +0200

    Fixed typo that caused linking errors.

--- src/wrappers/gh_hal/backends/libgpiod/chip-api.cpp
+++ src/wrappers/gh_hal/backends/libgpiod/chip-api.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #include <gh_hal/backends/libgpiod/chip-api.hpp>
 
-namespace gh_log::backends::libgpiod_impl {
+namespace gh_hal::backends::libgpiod_impl {
 
     std::unique_ptr<gpiod::chip> openChip(std::filesystem::path chipPath) {
         return std::make_unique<gpiod::chip>(chipPath);
