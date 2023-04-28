989b588fab241a6ba800c4f8d99cb342975fe1ed
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-28 19:16:34 +0200

    Added LineRequest inizial class definition.

--- src/wrappers/gh_hal/internal/line-request.hpp
+++ src/wrappers/gh_hal/internal/line-request.hpp
@@ -3,12 +3,21 @@
 
 #include <gh_hal/hardware-access/board-digital-pin.hpp>
 
+#ifdef USE_LIBGPIOD
+#include <gh_hal/backends/libgpiod/chip-api.hpp>
+#endif // USE_LIBGPIOD
+#include <gh_hal/backends/simulated/simulated-chip.hpp>
+#include <gh_hal/backends/simulated/simulated-digital-board-pin.hpp>
+
 // C++ STL
 #include <array>
+#include <vector>
+#include <memory>
 #include <tuple>
 #include <type_traits>
 #include <cstdint>
 #include <algorithm>
+#include <functional>
 
 namespace gh_hal::internal {
 
@@ -59,8 +68,30 @@ namespace gh_hal::internal {
         static_assert(
             LibgpiodConverter.convert(hardware_access::DigitalPinRequestDirection::Output) == ::gpiod::line::direction::OUTPUT);
 #endif // USE_LIBGPIOD
+
+        using FakeLineRequest = std::pair<hardware_access::DigitalPinRequestDirection, std::vector<backends::simulated::DigitalBoardPin>>;
+
     } // namespace details
 
+    class LineRequest final {
+    public:
+#ifdef USE_LIBGPIOD
+        using backend_type = backends::libgpiod_impl::NativeLineRequestType;
+        using chip_type = backends::libgpiod_impl::NativeChipType;
+#else
+        using backend_type = details::FakeLineRequest;
+        using chip_type = backends::simulated::SimulatedChip;
+#endif // USE_LIBGPIOD
 
+        using consumer_type = std::string;
+        using chip_reference = std::reference_wrapper<chip_type>;
+
+        explicit LineRequest(consumer_type consumer, chip_reference reference) noexcept;
+
+    private:
+        chip_reference m_chipRef;
+        consumer_type m_consumer{};
+        std::unique_ptr<backend_type> m_lineRequest{};
+    };
 
 } // namespace gh_hal::internal
