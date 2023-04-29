f610df076937d0d584142e9254bcbfcbefd106fe
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-29 18:24:25 +0200

    Updated documentation.

--- src/wrappers/gh_hal/hardware-access/board-digital-pin.hpp
+++ src/wrappers/gh_hal/hardware-access/board-digital-pin.hpp
@@ -7,21 +7,46 @@
 
 namespace gh_hal::hardware_access {
 
+    //!!
+    //! \brief Represents the direction of a line request, i.e. if the
+    //!  pins of the line request must be configured in output or input modes.
     enum class DigitalPinRequestDirection {
+        // The PIN's resource will read from the hardware PIN.
         Input,
+
+        // The PIN's resouce will write to the hardware PIN.
         Output
     };
 
+    //!!
+    //! \brief Represents the basic interface of a resource that is
+    //!  used to activate/deactivate or read to/from an hardware PIN on
+    //!  a board chip.
     struct BoardDigitalPin {
         using offset_type = std::uint32_t;
 
         virtual ~BoardDigitalPin() noexcept = default;
 
+        //!!
+        //! \brief Retrieves the direction of this digital pin.
+        //!
+        //! \return The direction of the digital pin.
         virtual DigitalPinRequestDirection getDirection() const noexcept = 0;
 
+        //!!
+        //! \brief If on the OUTPUT mode, it will activate the hardware's PIN.
+        //!
         virtual void activate() noexcept = 0;
+
+        //!!
+        //! \brief If on the OUTPUT mode, it will deactivate the hardware's PIN.
+        //!
         virtual void deactivate() noexcept = 0;
 
+        //!!
+        //! \brief Prints the status of the PIN (line number and direction) to the
+        //!  given output stream.
+        //!
         virtual void printStatus(std::ostream& ost) const noexcept = 0;
     };
 
