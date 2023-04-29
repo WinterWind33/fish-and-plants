82004bc184822c32c1594eb1b64d936eb8324952
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-04-29 17:00:08 +0200

    Added missing implementation of BoardChipMock class.

--- test/gh_hal/test-doubles/hardware-access/board-chip.mock.hpp
+++ test/gh_hal/test-doubles/hardware-access/board-chip.mock.hpp
@@ -3,10 +3,17 @@
 
 #include <gh_hal/hardware-access/board-chip.hpp>
 
+#ifdef USE_GMOCK
+#include <gmock/gmock.h>
+#endif // USE_GMOCK
+
 namespace gh_hal::hardware_access::mocks {
 
     class BoardChipMock : public BoardChip {
     public:
+        MOCK_METHOD((std::unique_ptr<BoardDigitalPin>), requestDigitalPin, (std::string, BoardDigitalPin::offset_type, const DigitalPinRequestDirection), (noexcept, final));
+        MOCK_METHOD((std::vector<std::unique_ptr<BoardDigitalPin>>), requestDigitalPinPool, (std::string, std::vector<BoardDigitalPin::offset_type>, const DigitalPinRequestDirection), (noexcept, final));
+        MOCK_METHOD(bool, releaseRequest, (std::vector<BoardDigitalPin::offset_type>), (noexcept, final));
     };
 
 } // namespace gh_hal::hardware_access::mocks
