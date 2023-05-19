8844c02a4d4bf6dcbe630776b44bb1f6b3d0650a
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-05-19 22:17:14 +0200

    Added first implementation of static analysis script.

--- scripts/static-analyze-files.sh
+++ scripts/static-analyze-files.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+
+for file in "$@";
+do
+    echo "[INFO] => Processing file $file"
+    clang-tidy -p buildLinux/compile_commands.json $file -extra-arg=-std=c++20 -- \
+        -DUSE_POPL20 -DUSE_LIBGPIOD -DUSE_SPDLOG \
+        -Isrc/rpi_gc -Isrc/wrappers/ \
+        -Isrc/third-party/popl20 -Isrc/third-party/libgpiod-2.0.1/include -Isrc/third-party/spdlog-1.11.0/include
+done
