612262d4178493e69a46f6105dd009f95e0575a0
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-25 22:57:50 +0200

    Begin substitute old versions definitions.

--- src/rpi_gc/version/version-numbers.hpp
+++ src/rpi_gc/version/version-numbers.hpp
@@ -7,12 +7,36 @@
 // C++ STL
 #include <cstdint>
 #include <string_view>
+#include <optional>
 
 namespace rpi_gc::version {
 
-    constexpr std::int32_t RPI_GC_VERSION_MAJOR{1};
-    constexpr std::int32_t RPI_GC_VERSION_MINOR{0};
-    constexpr std::int32_t RPI_GC_VERSION_PATCH{0};
-    constexpr std::string_view RPI_GC_VERSION_RC_NAME{""};
+    constexpr std::uint16_t RPI_GC_VERSION_MAJOR{1};
+    constexpr std::uint16_t RPI_GC_VERSION_MINOR{0};
+    constexpr std::uint16_t RPI_GC_VERSION_PATCH{0};
+    constexpr std::string_view RPI_GC_VERSION_RC_NAME{};
+
+    namespace details{
+        namespace prerelease {
+            constexpr std::optional<std::uint16_t> RPI_GC_VERSION_RC_NUM{};
+        } // namespace prerelease
+    } // namespace details
+
+    consteval semver::version getApplicationVersion() noexcept {
+        if constexpr (details::prerelease::RPI_GC_VERSION_RC_NUM.has_value()) {
+            return semver::version{
+                RPI_GC_VERSION_MAJOR,
+                RPI_GC_VERSION_MINOR,
+                RPI_GC_VERSION_PATCH,
+                semver::prerelease::rc,
+                details::prerelease::RPI_GC_VERSION_RC_NUM};
+        } else {
+
+            return semver::version{
+                RPI_GC_VERSION_MAJOR,
+                RPI_GC_VERSION_MINOR,
+                RPI_GC_VERSION_PATCH};
+        }
+    }
 
 } // namespace rpi_gc::version
