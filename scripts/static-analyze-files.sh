0b77e27c7e1db3be806a217ab768dd7f5e3419c6
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-25 22:44:17 +0200

    Added semver include to clang tidy.

--- scripts/static-analyze-files.sh
+++ scripts/static-analyze-files.sh
@@ -16,7 +16,8 @@ do
         clang-tidy -p buildLinux/compile_commands.json $file -extra-arg=-std=c++20 -- \
             -DUSE_POPL20 -DUSE_LIBGPIOD -DUSE_SPDLOG \
             -Isrc/rpi_gc -Isrc/wrappers/ \
-            -Isrc/third-party/popl20 -Isrc/third-party/libgpiod-2.0.1/include -Isrc/third-party/spdlog-1.11.0/include
+            -Isrc/third-party/popl20 -Isrc/third-party/libgpiod-2.0.1/include -Isrc/third-party/spdlog-1.11.0/include \
+            -Ithird-party/semver/include
     else
         echo "[INFO] => Skipping $file because it's not C++ code."
         continue
