41f3e2af5bab684f3189a35dca32a52d9d6c437e
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-26 22:27:09 +0200

    Added Project class.

--- src/modules/project-management/include/project-management/project.hpp
+++ src/modules/project-management/include/project-management/project.hpp
@@ -1,2 +1,17 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
+
+// Third-party
+#include <semver.hpp>
+
+namespace gc::project_management {
+
+    class Project final {
+    public:
+        constexpr Project() noexcept = default;
+
+    private:
+        semver::version m_projectVersion{};
+    };
+
+} // namespace gc::project_management
