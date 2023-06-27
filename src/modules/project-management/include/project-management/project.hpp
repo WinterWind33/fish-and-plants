822ea93aad1a184e142b022116e059ae7d96486d
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-27 08:21:43 +0200

    Added methods to the project class.

--- src/modules/project-management/include/project-management/project.hpp
+++ src/modules/project-management/include/project-management/project.hpp
@@ -4,14 +4,40 @@
 // Third-party
 #include <semver.hpp>
 
+// C++ STL
+#include <chrono>
+#include <string>
+
 namespace gc::project_management {
 
     class Project final {
     public:
-        constexpr Project() noexcept = default;
+        using time_point_type = std::chrono::time_point<std::chrono::system_clock>
+        using project_version = semver::version;
+        using project_title = std::string;
+
+        constexpr explicit Project(
+            time_point_type creationTime, project_title projectTitle, project_version version) noexcept :
+            m_creationTimePoint{std::move(creationTime)},
+            m_projectTitle{std::move(projectTitle)},
+            m_projectVersion{std::move(version)} {}
+
+        constexpr time_point_type getCreationTime() const noexcept {
+            return m_creationTimePoint;
+        }
+
+        constexpr project_title getTitle() const noexcept {
+            return m_projectTitle;
+        }
+
+        constexpr project_version getVersion() const noexcept {
+            return m_projectVersion;
+        }
 
     private:
-        semver::version m_projectVersion{};
+        const time_point_type m_creationTimePoint;
+        project_title m_projectTitle{};
+        project_version m_projectVersion{};
     };
 
 } // namespace gc::project_management
