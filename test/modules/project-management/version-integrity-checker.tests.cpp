5e31036fc8250cd7836c9f4327f12350ec63face
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-28 18:36:03 +0200

    Added first static tests.

--- test/modules/project-management/version-integrity-checker.tests.cpp
+++ test/modules/project-management/version-integrity-checker.tests.cpp
@@ -1,1 +1,15 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+#include <project-management/integrity-check/version-integrity-checker.hpp>
+
+#include <testing-core.hpp>
+
+TEST_CASE("VersionIntegrityChecker static tests", "[static][modules][project-management][integrity-check][VersionIntegrityChecker]") {
+    using namespace gc::project_management;
+
+    constexpr semver::version OLD_VERSION{1, 0, 0};
+    constexpr semver::version NEW_VERSION{2, 0, 0};
+    constexpr Project OLD_PROJECT{Project::time_point_type{}, "TestProject", OLD_VERSION};
+    constexpr integrity_check::VersionIntegrityChecker CHECKER_UNDER_TEST{NEW_VERSION};
+
+    STATIC_CHECK_FALSE(CHECKER_UNDER_TEST.checkIntegrity(OLD_PROJECT));
+}
