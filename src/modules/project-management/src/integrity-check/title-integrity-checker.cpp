2745d11133bf123da21130f3faeff4365760bfdb
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-30 18:44:21 +0200

    Added title-integrity-checker.cpp file.

---
+++ src/modules/project-management/src/integrity-check/title-integrity-checker.cpp
@@ -0,0 +1,4 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#include <project-management/integrity-check/title-integrity-checker.hpp>
+
+namespace gc::project_management::integrity_check {} // namespace gc::project_management::integrity_check
