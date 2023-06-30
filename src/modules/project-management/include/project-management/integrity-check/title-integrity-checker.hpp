9585612726cff1bd601ceebec23818ba8fc61f6d
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-30 17:14:32 +0200

    Added TitleIntegrityCheck class

--- src/modules/project-management/include/project-management/integrity-check/title-integrity-checker.hpp
+++ src/modules/project-management/include/project-management/integrity-check/title-integrity-checker.hpp
@@ -1,4 +1,32 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
 
-namespace gc::project_management::integrity_checker {} // namespace gc::project_management::integrity_checker
+#include <project-management/integrity-check/project-integrity-checker.hpp>
+#include <project-management/project.hpp>
+
+// C++ STL
+#include <string_view>
+
+namespace gc::project_management::integrity_check {
+
+    class TitleIntegrityChecker final : public ProjectIntegrityChecker {
+    public:
+        explicit constexpr TitleIntegrityChecker(Project::project_title defaultTitle) noexcept :
+            m_defaultTitle{defaultTitle} {}
+
+
+        bool checkIntegrity(const Project& project) const noexcept override {
+            return !project.getTitle().empty();
+        }
+
+        bool tryApplyIntegrityFixes(Project& project) override {
+            project = Project(project.getCreationTime(), m_defaultTitle, project.getVersion());
+
+            return true;
+        }
+
+    private:
+        Project::project_title m_defaultTitle{};
+    };
+
+} // namespace gc::project_management::integrity_check
