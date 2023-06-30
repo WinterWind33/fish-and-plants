cdbd6f097941ab18b7091c6dc9ca71e3bc3678ba
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-30 19:01:43 +0200

    Updated documentation 2.

--- src/modules/project-management/include/project-management/integrity-check/version-integrity-checker.hpp
+++ src/modules/project-management/include/project-management/integrity-check/version-integrity-checker.hpp
@@ -8,16 +8,34 @@
 
 namespace gc::project_management::integrity_check {
 
+    //!!
+    //! \brief Represents the checker of the project version.
+    //!
     class VersionIntegrityChecker final : public ProjectIntegrityChecker {
     public:
+
+        //!!
+        //! \brief Constructs a new checker object with the given version. This version will
+        //!  be used to check the project versions.
         constexpr explicit VersionIntegrityChecker(semver::version version) noexcept :
             m_newVersion{version} {}
 
+        //!!
+        //! \brief Checks whether or not the given project has the same version
+        //!  of the default one.
+        //!
+        //! \param project The project whose version eed to be checked.
+        //! \return True if the version is up to date, false otherwise.
         [[nodiscard]]
         constexpr bool checkIntegrity(const Project& project) const noexcept override {
             return m_newVersion == project.getVersion();
         }
 
+        //!!
+        //! \brief Applies the default version to the given project.
+        //!
+        //! \param project The project whose version need to be updated.
+        //! \return bool True, always.
         [[nodiscard]]
         bool tryApplyIntegrityFixes(Project& project) override;
 
