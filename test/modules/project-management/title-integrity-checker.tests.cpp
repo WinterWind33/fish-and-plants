4467847805ff09bdd4047dbb7a197a7a7fdd391f
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-06-30 18:14:06 +0200

    Added unit tests.

--- test/modules/project-management/title-integrity-checker.tests.cpp
+++ test/modules/project-management/title-integrity-checker.tests.cpp
@@ -12,3 +12,43 @@ TEST_CASE("TitleIntegrityChecker static tests", "[static][modules][project-manag
 
     STATIC_CHECK_FALSE(checkerUnderTest.checkIntegrity(TEST_PROJECT));
 }
+
+TEST_CASE("TitleIntegrityChecker unit tests", "[unit][solitary][modules][project-management][integrity-check][TitleIntegrityChecker]") {
+    using namespace gc::project_management;
+
+    constexpr std::string_view EXPECTED_TITLE{"DefaultTitle"};
+    integrity_check::TitleIntegrityChecker checkerUnderTest{EXPECTED_TITLE};
+
+    GIVEN("A project without a title") {
+        Project project{Project::time_point_type{}, "", semver::version{1, 0, 0}};
+
+        WHEN("The title is updated") {
+            const bool bRes{checkerUnderTest.tryApplyIntegrityFixes(project)};
+
+            THEN("The title should be the default one") {
+                CHECK(project.getTitle() == EXPECTED_TITLE);
+            }
+
+            THEN("The execution should succeed") {
+                CHECK(bRes);
+            }
+        }
+    }
+
+    GIVEN("A project with a title") {
+        constexpr std::string_view VALID_TITLE{"ValidTitle"};
+        Project project{Project::time_point_type{}, VALID_TITLE, semver::version{1, 0, 0}};
+
+        WHEN("The title is updated") {
+            const bool bRes{checkerUnderTest.tryApplyIntegrityFixes(project)};
+
+            THEN("The title should be the default one") {
+                CHECK(project.getTitle() == EXPECTED_TITLE);
+            }
+
+            THEN("The execution should succeed") {
+                CHECK(bRes);
+            }
+        }
+    }
+}
