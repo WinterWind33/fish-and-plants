de7c80db64a1e9954e69a426adfc5844f7b58169
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-01 08:52:18 +0200

    Added nodiscard.

--- src/rpi_gc/commands/project-command.hpp
+++ src/rpi_gc/commands/project-command.hpp
@@ -3,11 +3,20 @@
 
 #include <commands/terminal-command.hpp>
 
+#include <user-interface/commands-strings.hpp>
+
 namespace rpi_gc::commands {
 
     class ProjectCommand final : public TerminalCommandType {
     public:
 
+        //!!
+        //! \brief Retrieves the name of this command: project.
+        //!
+        [[nodiscard]]
+        constexpr name_type getName() const noexcept override {
+            return strings::commands::PROJECT;
+        }
     };
 
 } // namespace rpi_gc::commands
