edfa9792791306f8af329d259d559ca88bbf00d6
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-07 21:14:20 +0200

    Implemented first version of the project controller.

--- src/rpi_gc/gc-project/project-controller.hpp
+++ src/rpi_gc/gc-project/project-controller.hpp
@@ -1,2 +1,30 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
+
+#include <project-management/project.hpp>
+
+// C++ STL
+#include <optional>
+
+namespace rpi_gc::gc_project {
+
+    class ProjectController final {
+    public:
+        using project_type = gc::project_management::Project;
+
+        bool hasProject() const noexcept {
+            return m_currentProject.has_value();
+        }
+
+        const project_type& getCurrentProject() const {
+            return m_currentProject.value();
+        }
+
+        void setCurrentProject(project_type project);
+        void closeCurrentProject() noexcept;
+
+    private:
+        std::optional<project_type> m_currentProject{};
+    };
+
+} // namespace rpi_gc::gc_project
