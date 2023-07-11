eccd0f94731a40d56d2f619608f85dbf0ac11005
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-11 21:18:46 +0200

    Added json writer implementation.

--- src/modules/project-management/src/project-io/json-project-writer.cpp
+++ src/modules/project-management/src/project-io/json-project-writer.cpp
@@ -1,2 +1,31 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #include <project-io/json-project-writer.hpp>
+
+// Third-party
+#include <nlohmann/json.hpp>
+
+// C++ STL
+#include <sstream>
+
+namespace gc::project_management::project_io {
+
+    JsonProjectWriter::JsonProjectWriter(std::ostream& outputStream) noexcept :
+        m_outputStream{outputStream} {}
+
+    void JsonProjectWriter::serializeProject(const Project& project) {        ;
+        nlohmann::json projectJson{};
+
+        std::ostringstream creationTimeStream{};
+        creationTimeStream << project.getCreationTime();
+
+        projectJson =
+        {
+            {"version", project.getVersion().to_string()},
+            {"title", project.getTitle()},
+            {"creation_timedate", creationTimeStream.str()}
+        };
+
+        m_outputStream.get() << projectJson;
+    }
+
+} // namespace gc::project_management::project_io
--- src/modules/project-management/src/project-io/json-project-writer.hpp
+++ src/modules/project-management/src/project-io/json-project-writer.hpp
@@ -3,14 +3,20 @@
 
 #include "project-management/project-io/project-writer.hpp"
 
+// C++ STL
+#include <ostream>
+#include <functional> // for std::reference_wrapper
+
 namespace gc::project_management::project_io {
 
     class JsonProjectWriter final : public ProjectWriter {
     public:
+        explicit JsonProjectWriter(std::ostream& outputStream) noexcept;
 
         void serializeProject(const Project& project) override;
 
     private:
+        std::reference_wrapper<std::ostream> m_outputStream;
     };
 
 } // namespace gc::project_management::project_io
