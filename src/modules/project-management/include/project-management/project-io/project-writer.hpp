4270aff4957e9e4d7e75df3725eb8575290996e0
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-11 20:34:25 +0200

    Added ProjectWriter interface.

--- src/modules/project-management/include/project-management/project-io/project-writer.hpp
+++ src/modules/project-management/include/project-management/project-io/project-writer.hpp
@@ -1,2 +1,30 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
+
+#include <project-management/project.hpp>
+
+// C++ STL
+#include <memory>
+
+namespace gc::project_management::project_io {
+
+    //!!
+    //! \brief Represents the interface of a project writer. This serializes or
+    //!  sends the project structure to the desired sink.
+    //!
+    struct ProjectWriter {
+        virtual ~ProjectWriter() noexcept = default;
+
+        //!!
+        //! \brief Serializes the project. Can throw exceptions if the serialization fails.
+        //!
+        //! \param project The project to serialize.
+        virtual void serializeProject(const Project& project) = 0;
+    };
+
+    inline ProjectWriter& operator<<(ProjectWriter& writer, const Project& project) {
+        writer.serializeProject(project);
+        return writer;
+    }
+
+} // namespace gc::project_management::project_io
