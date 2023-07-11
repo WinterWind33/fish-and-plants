8325fea1f3bfbd946a46c61fa98009def98873e2
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-11 20:44:14 +0200

    Added json-project-writer.cpp file.

---
+++ src/modules/project-management/src/project-io/json-project-writer.cpp
@@ -0,0 +1,2 @@
+// Copyright (c) 2023 Andrea Ballestrazzi
+#include <project-io/json-project-writer.hpp>
--- src/modules/project-management/src/project-io/json-project-writer.hpp
+++ src/modules/project-management/src/project-io/json-project-writer.hpp
@@ -1,2 +1,16 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
+
+#include "project-management/project-io/project-writer.hpp"
+
+namespace gc::project_management::project_io {
+
+    class JsonProjectWriter final : public ProjectWriter {
+    public:
+
+        void serializeProject(const Project& project) override;
+
+    private:
+    };
+
+} // namespace gc::project_management::project_io
