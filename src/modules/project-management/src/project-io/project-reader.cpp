b247e0607a2380102401f11c9c826baa1bad92d5
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-17 19:38:15 +0200

    Made integration test pass.

--- src/modules/project-management/src/project-io/project-reader.cpp
+++ src/modules/project-management/src/project-io/project-reader.cpp
@@ -7,9 +7,12 @@
 namespace gc::project_management::project_io {
 
 std::unique_ptr<ProjectReader> CreateJsonProjectFileReader(const std::filesystem::path& path) {
-    if(!std::filesystem::exists(path)) {
+    if(!std::filesystem::exists(path))
         throw std::invalid_argument{"The specified path does not exist."};
-    }
+
+    if(!std::filesystem::is_regular_file(path))
+        throw std::invalid_argument{"The specified path is not a valid JSON file."};
+
     return nullptr;
 }
 
