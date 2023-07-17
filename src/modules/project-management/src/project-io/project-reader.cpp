063177ec3bf6ab960207c03a8205fdaa52ddf0e1
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-17 19:13:48 +0200

    Made factory function throw on error.

--- src/modules/project-management/src/project-io/project-reader.cpp
+++ src/modules/project-management/src/project-io/project-reader.cpp
@@ -1,9 +1,15 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #include <project-management/project-io/project-reader.hpp>
 
+// C++ STL
+#include <stdexcept>
+
 namespace gc::project_management::project_io {
 
 std::unique_ptr<ProjectReader> CreateJsonProjectFileReader(const std::filesystem::path& path) {
+    if(!std::filesystem::exists(path)) {
+        throw std::invalid_argument{"The specified path does not exist."};
+    }
     return nullptr;
 }
 
