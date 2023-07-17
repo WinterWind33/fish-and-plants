21fda27a31783ccb4a6e6cb94bfddd3313dc904a
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-17 18:06:35 +0200

    Make unit test compile.

--- src/modules/project-management/src/project-io/json-project-reader.cpp
+++ src/modules/project-management/src/project-io/json-project-reader.cpp
@@ -1,1 +1,9 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+#include <project-io/json-project-reader.hpp>
+
+namespace gc::project_management::project_io {
+
+    JsonProjectReader::JsonProjectReader(std::istream& ist) noexcept :
+        m_inputStream{ist} {}
+
+} // namespace gc::project_management::project_io
--- src/modules/project-management/src/project-io/json-project-reader.hpp
+++ src/modules/project-management/src/project-io/json-project-reader.hpp
@@ -1,4 +1,23 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
 
-namespace gc::project_management::project_io {} // namespace gc::project_management::project_io
+#include <project-management/project.hpp>
+
+// C++ STL
+#include <istream>
+
+namespace gc::project_management::project_io {
+
+    class JsonProjectReader final {
+    public:
+        explicit JsonProjectReader(std::istream& inputStream) noexcept;
+
+    private:
+        std::istream& m_inputStream;
+    };
+
+    inline JsonProjectReader& operator>>(JsonProjectReader& reader, Project& inputProject) {
+        return reader;
+    }
+
+} // namespace gc::project_management::project_io
