3e31676e3785b29f5d4abd3e1e6a5286d13fe6c5
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-21 18:58:58 +0200

    Added ProjectComponentMock class.

--- test/rpi_gc/test-doubles/gc-project/project-component.mock.hpp
+++ test/rpi_gc/test-doubles/gc-project/project-component.mock.hpp
@@ -1,4 +1,14 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
 
-namespace rpi_gc::gc_project::mocks {} // namespace rpi_gc::gc_project::mocks
+#include <gc-project/project-component.hpp>
+#include <gmock/gmock.h>
+
+namespace rpi_gc::gc_project::mocks {
+
+    class ProjectComponentMock : public ProjectComponent {
+    public:
+        MOCK_METHOD(void, saveToProject, (const gc::project_management::Project&), (final));
+    };
+
+} // namespace rpi_gc::gc_project::mocks
