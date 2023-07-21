9b86d790b033b04285583e0177f3ee0578cc564a
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-21 18:48:22 +0200

    Added ProjectComponent interface.

--- src/rpi_gc/gc-project/project-component.hpp
+++ src/rpi_gc/gc-project/project-component.hpp
@@ -1,4 +1,21 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
 
-namespace rpi_gc::gc_project {} // namespace rpi_gc::gc_project
+#include <project-management/project.hpp>
+
+namespace rpi_gc::gc_project {
+
+    //!!
+    //! \brief Interface implemented by all of that stateful classes that
+    //!  can or need to save data into the Greenhouse CAD project.
+    struct ProjectComponent {
+        virtual ~ProjectComponent() noexcept = default;
+
+        //!!
+        //! \brief Saves the component data inside the given project.
+        //!
+        //! \param project The project which will receive the component data.
+        virtual void saveToProject(const gc::project_management::Project& project) = 0;
+    };
+
+} // namespace rpi_gc::gc_project
