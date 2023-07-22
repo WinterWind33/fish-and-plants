3568a578daf65f4030517c1541840af737e0aa89
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-22 17:47:05 +0200

    Added first read implementation.

--- src/modules/project-management/src/project-io/json-project-reader.cpp
+++ src/modules/project-management/src/project-io/json-project-reader.cpp
@@ -31,8 +31,52 @@ namespace gc::project_management::project_io {
             projectVersionStr = "0.0.0";
 
         semver::version projectVersion{semver::from_string(projectVersionStr)};
+        Project finalProject{creationTimeDate, std::move(projectTitle), projectVersion};
 
-        return Project{creationTimeDate, std::move(projectTitle), projectVersion};
+        // Now that we have read all the basic project information, we can read the project's values,
+        // value arrays and objects.
+        for(const auto& [key, value] : inputProjectJson.items()) {
+            // Skip the project header.
+            if(key == "creation_timedate" || key == "title" || key == "version")
+                continue;
+
+            // If the value is an array, we read it as an array of values.
+            if(value.is_array()) {
+                std::vector<ProjectNode::value_impl_type> valueArray{};
+                for(const auto& arrayValue : value) {
+                    // We need to instance a variant based on the array value.
+                    ProjectNode::value_impl_type finalValue{};
+                    valueArray.push_back(std::move(finalValue));
+                }
+                finalProject.addValueArray(key, std::move(valueArray));
+            }
+
+            // If the value is an object, we read it as an object of values.
+            else if(value.is_object()) {
+                ProjectNode valueObject{};
+                finalProject.addObject(key, std::move(valueObject));
+            }
+            // If the value is a simple value, we read it as a simple value.
+            else {
+                if(value.is_boolean()) {
+                    finalProject.addValue(key, value.get<bool>());
+                }
+                else if(value.is_number_integer()) {
+                    finalProject.addValue(key, value.get<std::int64_t>());
+                }
+                else if(value.is_number_unsigned()) {
+                    finalProject.addValue(key, value.get<std::uint64_t>());
+                }
+                else if(value.is_number_float()) {
+                    finalProject.addValue(key, value.get<double>());
+                }
+                else if(value.is_string()) {
+                    finalProject.addValue(key, value.get<std::string>());
+                }
+            }
+        }
+
+        return finalProject;
     }
 
 } // namespace gc::project_management::project_io
