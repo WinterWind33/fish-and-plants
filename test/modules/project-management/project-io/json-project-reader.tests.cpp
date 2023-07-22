7b37870c645a355eedfa9cb02aece74bb92d0bd4
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-22 17:19:04 +0200

    Added unit test on project value reading.

--- test/modules/project-management/project-io/json-project-reader.tests.cpp
+++ test/modules/project-management/project-io/json-project-reader.tests.cpp
@@ -12,7 +12,7 @@
 TEST_CASE("JsonProjectReader unit tests", "[unit][sociable][project-management][project-io][JsonProjectReader]") {
     using namespace gc::project_management;
 
-    GIVEN("An project with invalid version") {
+    GIVEN("A project with invalid version") {
         // The time point represents the 01/01/2023 12:30:40 PM timedate.
         auto inputStream{std::make_unique<std::istringstream>(R"(
             {
@@ -56,6 +56,34 @@ TEST_CASE("JsonProjectReader unit tests", "[unit][sociable][project-management][
 
             THEN("The read project should be correct") {
                 CHECK(SoftCompareProjects(inputProject, expectedProject));
+            }
+        }
+    }
+
+    GIVEN("A project with a value") {
+        auto inputStream{std::make_unique<std::istringstream>(R"(
+            {
+                "creation_timedate": 1672576240,
+                "title": "test-title",
+                "version": "1.2.3",
+                "test-value": 42
+            }
+        )")};
+
+        Project expectedProject{std::chrono::system_clock::from_time_t(1672576240), "test-title", semver::version{1, 2, 3}};
+        expectedProject.addValue("test-value", 42);
+
+        WHEN("The project is read") {
+            project_io::JsonProjectReader projectReaderUnderTest{std::move(inputStream)};
+            Project inputProject{};
+            projectReaderUnderTest >> inputProject;
+
+            THEN("The read project basic data should be correct") {
+                REQUIRE(SoftCompareProjects(inputProject, expectedProject));
+
+                AND_THEN("The read project should have the correct value") {
+                    CHECK(inputProject.getValue<std::int64_t>("test-value") == 42);
+                }
             }
         }
     }
