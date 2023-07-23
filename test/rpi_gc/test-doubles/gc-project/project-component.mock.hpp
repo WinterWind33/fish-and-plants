b2c6f9abb3b714fec82e7f91796fd6f2806544bc
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-23 09:32:53 +0200

    Added loadConfigFromProject method to ProjectComponent.

--- test/rpi_gc/test-doubles/gc-project/project-component.mock.hpp
+++ test/rpi_gc/test-doubles/gc-project/project-component.mock.hpp
@@ -9,6 +9,7 @@ namespace rpi_gc::gc_project::mocks {
     class ProjectComponentMock : public ProjectComponent {
     public:
         MOCK_METHOD(void, saveToProject, (gc::project_management::Project&), (final));
+        MOCK_METHOD(void, loadConfigFromProject, (const gc::project_management::Project&), (final));
     };
 
 } // namespace rpi_gc::gc_project::mocks
