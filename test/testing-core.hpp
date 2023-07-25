fd108e3503824f9f43d8ef9d5505dea5c8767ccf
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-25 21:34:02 +0200

    Updated clang-format and removed old include guard.

--- test/testing-core.hpp
+++ test/testing-core.hpp
@@ -1,12 +1,9 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
-#ifndef TESTING_CORE_HPP
-#define TESTING_CORE_HPP
+#pragma once
 
 #if USE_CATCH2_AS_TESTING_FRAMEWORK
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #else
 #error "No core framework support for testing apart from Catch2"
-#endif  // USE_CATCH2_AS_TESTING_FRAMEWORK
-
-#endif  // !TESTING_CORE_HPP
+#endif // USE_CATCH2_AS_TESTING_FRAMEWORK
