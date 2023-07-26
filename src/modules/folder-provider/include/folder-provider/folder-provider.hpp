df60aedd3182bb9b44c91645b919a53c85547c4f
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-26 23:22:20 +0200

    Added FolderProvider interface.

--- src/modules/folder-provider/include/folder-provider/folder-provider.hpp
+++ src/modules/folder-provider/include/folder-provider/folder-provider.hpp
@@ -1,4 +1,23 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
 
-namespace gc::folder_provider {} // namespace gc::folder_provider
+// C++ STL
+#include <filesystem>
+
+namespace gc::folder_provider {
+
+//!!
+//! \brief Interface for a folder provider. It retrieves
+//!  the utilities OS folders independently from the platform.
+struct FolderProvider {
+    virtual ~FolderProvider() noexcept = default;
+
+    //!!
+    //! \brief Retrieves the "AppData" folder, i.e. the folder
+    //!  where the application can store its data.
+    //!
+    //! \return The path to the "AppData" folder.
+    virtual std::filesystem::path getAppDataFolder() const = 0;
+};
+
+} // namespace gc::folder_provider
