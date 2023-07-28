f0b96b3487abc6beb5006ea3f78f73d1917c6f7f
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-28 18:24:28 +0200

    Added InitialProjectLoader.

--- src/rpi_gc/initial-project-loader.cpp
+++ src/rpi_gc/initial-project-loader.cpp
@@ -1,3 +1,11 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+#include <initial-project-loader.hpp>
 
-namespace rpi_gc {} // namespace rpi_gc
+namespace rpi_gc {
+
+InitialProjectLoader::InitialProjectLoader(
+    gh_log::Logger& logger, gc::folder_provider::FolderProvider& folder_provider) noexcept
+    : m_logger(logger),
+      m_folder_provider(folder_provider) {}
+
+} // namespace rpi_gc
--- src/rpi_gc/initial-project-loader.hpp
+++ src/rpi_gc/initial-project-loader.hpp
@@ -1,4 +1,26 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #pragma once
 
-namespace rpi_gc {} // namespace rpi_gc
+#include <folder-provider/folder-provider.hpp>
+#include <gh_log/logger.hpp>
+
+// C++ STL
+#include <functional> // for std::reference_wrapper
+
+namespace rpi_gc {
+
+//!!
+//! \brief Represents an object that will read the project to
+//!  load at startup from a file stored in the default.
+//!  app data folder of the operating system.
+class InitialProjectLoader final {
+public:
+    explicit InitialProjectLoader(gh_log::Logger& logger,
+                                  gc::folder_provider::FolderProvider& folder_provider) noexcept;
+
+private:
+    std::reference_wrapper<gh_log::Logger> m_logger;
+    std::reference_wrapper<gc::folder_provider::FolderProvider> m_folder_provider;
+};
+
+} // namespace rpi_gc
