26b804ff010f509d4a6a33a4b966311e5c6ec99a
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-28 15:03:45 +0200

    Adde initial test implementation.

--- test/rpi_gc/gc-project/project-config-loader.tests.cpp
+++ test/rpi_gc/gc-project/project-config-loader.tests.cpp
@@ -1,1 +1,17 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
+#include <gc-project/project-config-loader.hpp>
+
+// Test Doubles
+#include <gh_log/test-doubles/logger.mock.hpp>
+
+#include <testing-core.hpp>
+
+TEST_CASE("ProjectConfigLoader unit tests",
+          "[unit][sociable][rpi_gc][gc-project][ProjectConfigLoader]") {
+    using namespace rpi_gc::gc_project;
+
+    // Stub logger
+    ::testing::NiceMock<gh_log::mocks::LoggerMock> stubLogger{};
+
+    ProjectConfigLoader projectConfigLoaderUnderTest{stubLogger};
+}
