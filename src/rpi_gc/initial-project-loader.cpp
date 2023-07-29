0bd5e7a932b999f77b9e14554fffe63a250ae061
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-07-29 09:15:47 +0200

    Added implementation of initial project loader and updated CMake dependencies.

--- src/rpi_gc/initial-project-loader.cpp
+++ src/rpi_gc/initial-project-loader.cpp
@@ -1,11 +1,145 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 #include <initial-project-loader.hpp>
 
+#include <nlohmann/json.hpp>
+#include <project-management/project-io/project-reader.hpp>
+#include <version/version-numbers.hpp>
+
+// C++ STL
+#include <filesystem>
+#include <fstream>
+
 namespace rpi_gc {
+
+namespace details {
+
+// Logs the error if the given error code is not valid.
+[[nodiscard]] bool logErrorIfNotValid(std::error_code& errorCode, gh_log::Logger& logger) noexcept {
+    const bool bRes{static_cast<bool>(errorCode)};
+    if (!bRes) {
+        logger.logError("Error while trying to load project config. Error code: " +
+                        std::to_string(errorCode.value()));
+        logger.logError("Error message: " + errorCode.message());
+    }
+
+    return bRes;
+}
+
+// Creates the standard config file
+void createStandardConfigFile(const std::filesystem::path& configFilePath,
+                              gh_log::Logger& logger) noexcept {
+    std::ofstream configFile{configFilePath};
+    if (!configFile.is_open()) {
+        logger.logError("Error while trying to create the config file");
+        return;
+    }
+
+    // Now we create the JSON to write to the config file.
+    // We serialize the application version.
+    nlohmann::json configJson{};
+    configJson["version"] = rpi_gc::version::getApplicationVersion().to_string();
+
+    // Write out the JSON to the config file.
+    configFile << configJson.dump(4);
+}
+
+} // namespace details
 
 InitialProjectLoader::InitialProjectLoader(
     gh_log::Logger& logger, gc::folder_provider::FolderProvider& folder_provider) noexcept
-    : m_logger(logger),
-      m_folder_provider(folder_provider) {}
+    : m_logger{logger},
+      m_folderProvider{folder_provider} {}
+
+std::optional<std::pair<gc::project_management::Project, std::filesystem::path>>
+InitialProjectLoader::tryLoadCachedProject() noexcept {
+    using namespace gc::project_management;
+    m_logger.get().logInfo("Loading cached project");
+
+    const auto rpigcFolder = m_folderProvider.get().getAppDataFolder() /
+                             std::filesystem::path{"FishAndPlants"} /
+                             std::filesystem::path{"rpi_gc"};
+
+    auto configFilePath = rpigcFolder / std::filesystem::path{"rpi_gc_config.json"};
+
+    std::error_code errorCode{};
+    if (!std::filesystem::exists(rpigcFolder, errorCode)) {
+        m_logger.get().logWarning("Application data folder was not found. Creating it...");
+
+        // If the error code is not zero then there was a system error.
+        if (!details::logErrorIfNotValid(errorCode, m_logger.get())) {
+            return std::nullopt;
+        }
+
+        // We create the folder and the config file.
+        std::filesystem::create_directories(rpigcFolder, errorCode);
+        if (!details::logErrorIfNotValid(errorCode, m_logger.get())) {
+            return std::nullopt;
+        }
+
+        details::createStandardConfigFile(configFilePath, m_logger.get());
+        return std::nullopt;
+    }
+
+    // If we are here this means that the application data folder exists. We
+    // must check if the configuration file exists as well.
+    if (!std::filesystem::exists(configFilePath, errorCode)) {
+        m_logger.get().logWarning("Application config file was not found. Creating it...");
+
+        // If the error code is not zero then there was a system error.
+        if (!details::logErrorIfNotValid(errorCode, m_logger.get())) {
+            return std::nullopt;
+        }
+
+        details::createStandardConfigFile(configFilePath, m_logger.get());
+        return std::nullopt;
+    }
+
+    // If we are here this means that the application data folder exists and
+    // the application config file as well. We can load the information about
+    // the last opened project location.
+    std::ifstream configFile{configFilePath};
+    if (!configFile.is_open()) {
+        m_logger.get().logError("Error while trying to open the config file");
+        return std::nullopt;
+    }
+
+    // Now we parse the JSON.
+    nlohmann::json configJson{};
+    configFile >> configJson;
+
+    // We check if the version is the same as the current one.
+    const auto version = configJson["version"].get<std::string>();
+
+    // If the version is an old one we update it to the last one.
+    if (version != rpi_gc::version::getApplicationVersion().to_string()) {
+        m_logger.get().logWarning(
+            "The application version is different from the one in the config file. Updating it...");
+        configJson["version"] = rpi_gc::version::getApplicationVersion().to_string();
+    }
+
+    // We check if the project location is present.
+    if (!configJson.contains("lastProjectLocation")) {
+        m_logger.get().logWarning("Project location not found in the config file");
+        return std::nullopt;
+    }
+
+    // We check if the project location is valid.
+    const auto projectLocation =
+        std::filesystem::path{configJson["lastProjectLocation"].get<std::string>()};
+    if (!std::filesystem::exists(projectLocation, errorCode)) {
+        m_logger.get().logWarning("Project location not found");
+        return std::nullopt;
+    }
+
+    // If we are here this means that the project location is valid. We can
+    // load the project. We load it using a JSON project reader.
+    Project project{};
+
+    auto projectReader{project_io::CreateJsonProjectFileReader(projectLocation)};
+    *projectReader >> project;
+
+    m_logger.get().logInfo("Project loaded successfully");
+    return std::make_pair(project, projectLocation);
+}
 
 } // namespace rpi_gc
--- src/rpi_gc/initial-project-loader.hpp
+++ src/rpi_gc/initial-project-loader.hpp
@@ -3,9 +3,12 @@
 
 #include <folder-provider/folder-provider.hpp>
 #include <gh_log/logger.hpp>
+#include <project-management/project.hpp>
 
 // C++ STL
 #include <functional> // for std::reference_wrapper
+#include <optional>
+#include <tuple> // for std::pair
 
 namespace rpi_gc {
 
@@ -18,9 +21,23 @@ public:
     explicit InitialProjectLoader(gh_log::Logger& logger,
                                   gc::folder_provider::FolderProvider& folder_provider) noexcept;
 
+    //!!
+    //! \brief Searches for the app data folder it the operating system folders
+    //!  and tries to load the project from the config file stored in it.
+    //!  If the application folder or the config file is not found, this function will create
+    //!  them and return an empty optional.
+    //!
+    //!  If a config file is found then it will be parsed and it will search for the last
+    //!  project that was opened and it will try to load it.
+    //!
+    //! \return An optional containing the project if it was loaded successfully.
+    //!  Otherwise an empty optional.
+    std::optional<std::pair<gc::project_management::Project, std::filesystem::path>>
+    tryLoadCachedProject() noexcept;
+
 private:
     std::reference_wrapper<gh_log::Logger> m_logger;
-    std::reference_wrapper<gc::folder_provider::FolderProvider> m_folder_provider;
+    std::reference_wrapper<gc::folder_provider::FolderProvider> m_folderProvider;
 };
 
 } // namespace rpi_gc
