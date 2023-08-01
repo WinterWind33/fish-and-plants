6a4bec5c991b28c71e2cf5e859f0a3c1e2f0e156
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-08-01 21:55:28 +0200

    Added last project update when saving a project.

--- src/rpi_gc/application-configuration.hpp
+++ src/rpi_gc/application-configuration.hpp
@@ -5,6 +5,7 @@
 
 // Third-party
 #include <folder-provider/folder-provider.hpp>
+#include <nlohmann/json.hpp>
 #include <semver.hpp>
 
 // C++ STL
@@ -15,33 +16,32 @@ namespace rpi_gc {
 
 struct ApplicationConfiguration {
     semver::version applicationVersion;
-    std::filesystem::path lastProjectFolder;
+    std::filesystem::path lastProjectLocation;
 };
 
 inline auto operator<<(std::ostream& os, const ApplicationConfiguration& applicationConfiguration)
     -> std::ostream& {
     // We write the application configuration in a JSON format to the given
     // output stream.
-    os << R"(
-        {
-            "version": ")"
-       << applicationConfiguration.applicationVersion.to_string() << R"(",
-            "lastProjectFolder": ")"
-       << applicationConfiguration.lastProjectFolder.string() << R"("
-        }
-    )" << std::endl;
+    nlohmann::json applicationConfigurationJson{};
+    applicationConfigurationJson["version"] =
+        applicationConfiguration.applicationVersion.to_string();
+    applicationConfigurationJson["lastProjectLocation"] =
+        applicationConfiguration.lastProjectLocation.string();
 
+    os << applicationConfigurationJson.dump(4);
     return os;
 }
 
 //!!
 //! \brief Creates the default application configuration with the
 //!  project version and the given folder as the last project folder.
-//! \param lastProjectFolder The last project folder.
+//! \param lastProjectLocation The last project folder.
 //!
 [[nodiscard]] inline auto CreateDefaultApplicationConfiguration(
-    std::filesystem::path lastProjectFolder) noexcept -> ApplicationConfiguration {
-    return ApplicationConfiguration{version::getApplicationVersion(), std::move(lastProjectFolder)};
+    std::filesystem::path lastProjectLocation) noexcept -> ApplicationConfiguration {
+    return ApplicationConfiguration{version::getApplicationVersion(),
+                                    std::move(lastProjectLocation)};
 }
 
 //!!
