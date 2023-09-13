be5100ef3ce957c89ed3e0c134615604ae78dcef
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-09-13 21:40:39 +0200

    Separated release scripts.

---
+++ scripts/aarch64-linux-perform-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# aarch64 cross compile on linux
+echo "[INFO] => Compiling for Linux aarch64"
+cd ../buildLinux
+rm -r -f ./*
+rm -r -f ../src/third-party/libgpiod-*
+rm ../bin/rpi_gc*
+cmake .. -DCOMPILE_AARCH64_ON_LINUX=True --toolchain "toolchains/aarch64-toolchain.cmake" -DCMAKE_TOOLCHAIN_FILE=$1 -DRPI_GC_BUILD_TESTS=OFF
+cmake --build . --target rpi_gc --config Release -j8
+echo "[INFO] => Copying to bin/release/aarch64-linux"
+mkdir -p ../bin/Release/aarch64-linux
+cp ../bin/rpi_gc* ../bin/Release/aarch64-linux
