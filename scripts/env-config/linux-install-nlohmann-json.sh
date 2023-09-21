91e386942d63783d33587c074d045c8f99ffa6fe
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-09-21 21:34:36 +0200

    Typos and fixes.

--- scripts/env-config/linux-install-nlohmann-json.sh
+++ scripts/env-config/linux-install-nlohmann-json.sh
@@ -4,6 +4,7 @@
 wget https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp
 
 # Copy the header file to the system-wide include directory
+sudo mkdir /usr/local/include/nlohmann
 sudo cp json.hpp /usr/local/include/nlohmann/json.hpp
 
 # Clean up
--- scripts/env-config/linux-install-spdlog.sh
+++ scripts/env-config/linux-install-spdlog.sh
@@ -3,7 +3,7 @@
 # Download the latest version of the spdlog library
 wget https://github.com/gabime/spdlog/archive/refs/tags/v1.12.0.tar.gz
 
-# Extract the downloaded archive to a directory of your choice
+# Extract the downloaded archive
 tar -xvf v1.12.0.tar.gz
 
 # Copy the header files to the system-wide include directory
