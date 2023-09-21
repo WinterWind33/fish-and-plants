f1b679186c6f328d160fcfabe4dfbf157ae479f8
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-09-21 21:28:21 +0200

    Typo.

--- scripts/env-config/linux-install-nlohmann-json.sh
+++ scripts/env-config/linux-install-nlohmann-json.sh
@@ -4,7 +4,7 @@
 wget https://github.com/nlohmann/json/releases/download/v3.11.2/json.hpp
 
 # Copy the header file to the system-wide include directory
-sudo cp json.hpp /usr/local/include/nlohmann/
+sudo cp json.hpp /usr/local/include/nlohmann/json.hpp
 
 # Clean up
 rm json.hpp
