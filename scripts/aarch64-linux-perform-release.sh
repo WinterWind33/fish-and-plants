af6a80e8975ed85177de3770f02938d6ef62a60f
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-09-21 20:52:28 +0200

    Updated aarch64 release build.

--- scripts/aarch64-linux-perform-release.sh
+++ scripts/aarch64-linux-perform-release.sh
@@ -6,7 +6,7 @@ cd ../buildLinux
 rm -r -f ./*
 rm -r -f ../src/third-party/libgpiod-*
 rm ../bin/rpi_gc*
-cmake .. -DCOMPILE_AARCH64_ON_LINUX=True --toolchain "toolchains/aarch64-toolchain.cmake" -DCMAKE_TOOLCHAIN_FILE=$1 -DRPI_GC_BUILD_TESTS=OFF
+cmake .. -DCOMPILE_AARCH64_ON_LINUX=True --toolchain "toolchains/aarch64-toolchain.cmake" -DRPI_GC_BUILD_TESTS=OFF
 cmake --build . --target rpi_gc --config Release -j8
 echo "[INFO] => Copying to bin/release/aarch64-linux"
 mkdir -p ../bin/Release/aarch64-linux
